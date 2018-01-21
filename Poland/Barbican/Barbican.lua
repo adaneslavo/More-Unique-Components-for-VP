082f7a6ee297eaaeba7f9087d0659e2f8ac3c70b
adan_eslavo <adan_eslavo@o2.pl>
2018-01-21 20:31:30 +0100

    v25.8
    Small visual tweaks. Changelog and credits update.

--- Poland/Barbican/Barbican.lua
+++ Poland/Barbican/Barbican.lua
@@ -5,14 +5,24 @@
 function GarrisonRangePower(iPlayer, iUnit, iX, iY)
 	-- check for Poland
 	local pPlayer = Players[iPlayer]
-	if not( pPlayer and pPlayer:GetCivilizationType() == GameInfoTypes.CIVILIZATION_POLAND ) then return end
+	
+	if not(pPlayer and pPlayer:GetCivilizationType() == GameInfoTypes.CIVILIZATION_POLAND) then 
+		return 
+	end
 	-- check for archer or siege
 	local pUnit = pPlayer:GetUnitByID(iUnit)
-	if not( pUnit and (pUnit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_ARCHER or pUnit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_SIEGE) ) then return end
+	
+	if not(pUnit and (pUnit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_ARCHER or pUnit:GetUnitCombatType() == GameInfoTypes.UNITCOMBAT_SIEGE)) then 
+		return 
+	end
 	-- process promo
 	local bHasBarbican = false
 	local pCity = pUnit:GetPlot():GetWorkingCity()
-	if pCity ~= nil then bHasBarbican = pCity:IsHasBuilding(GameInfoTypes.BUILDING_POLAND_BARBICAN) end
+	
+	if pCity ~= nil 
+		then bHasBarbican = pCity:IsHasBuilding(GameInfoTypes.BUILDING_POLAND_BARBICAN) 
+	end
+	
 	pUnit:SetHasPromotion(GameInfoTypes.PROMOTION_UNIT_POLAND_BARBICAN, bHasBarbican and pUnit:IsGarrisoned())
 end
 
