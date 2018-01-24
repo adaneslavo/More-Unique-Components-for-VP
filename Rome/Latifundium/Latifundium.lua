fab0b12cd8bfbfca83b432ffcdc9007ead9fb6f7
adan_eslavo <adan_eslavo@o2.pl>
2018-01-24 19:20:46 +0100

    v25.15
    Ave Ceasar!

--- Rome/Latifundium/Latifundium.lua
+++ Rome/Latifundium/Latifundium.lua
@@ -2,7 +2,7 @@
 -- Author: adan_eslavo
 -- DateCreated: 25/12/2017
 --------------------------------------------------------------
-include("ModUserData.lua")
+--[[include("ModUserData.lua")
 
 function CanRomeBuildPlantation (iPlayer, iUnit, iX, iY, iBuild)
 	if iBuild == GameInfoTypes.BUILD_PLANTATION and Players[iPlayer]:GetCivilizationType() == GameInfoTypes.CIVILIZATION_ROME then
@@ -33,8 +33,8 @@ function LatifundiumSpawnFigs(iPlayer)
 										modUserData.SetValue("FileToReset", "1");
 									end
 
-									for direction = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1, 1 do	
-										local pPlot = Map.PlotDirection(pMainPlot:GetX(), pMainPlot:GetY(), direction)
+									for iDirection = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1, 1 do	
+										local pPlot = Map.PlotDirection(pMainPlot:GetX(), pMainPlot:GetY(), iDirection)
 				
 										if (pPlot:GetTerrainType() == TerrainTypes.TERRAIN_GRASS or pPlot:GetTerrainType() == TerrainTypes.TERRAIN_PLAINS or pPlot:GetTerrainType() == TerrainTypes.TERRAIN_HILLS or pPlot:GetTerrainType() == TerrainTypes.TERRAIN_DESERT) then
 											if pPlot:GetResourceType(-1) == (-1) then
@@ -210,4 +210,128 @@ function GetNearestCity(pPlayer, pPlot)
 end
 
 GameEvents.PlayerDoTurn.Add(LatifundiumSpawnFigs)
-GameEvents.PlayerCanBuild.Add(CanRomeBuildPlantation)
+GameEvents.PlayerCanBuild.Add(CanRomeBuildPlantation)--]]
+local fGameSpeedModifier = GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].ConstructPercent / 100
+
+function LatifundiumSpawnFigs(iX, iY, iOwner, iOldImprovement, iNewImprovement, bPillaged)
+	local pPlayer = Players[iOwner]
+
+	if not (pPlayer and pPlayer:GetCivilizationType() == GameInfoTypes.CIVILIZATION_ROME) then return end
+	if not (iOldImprovement == GameInfoTypes.IMPROVEMENT_ROME_LATIFUNDIUM and iNewImprovement == GameInfoTypes.IMPROVEMENT_ROME_LATIFUNDIUM_UPGRADED) then return end
+				
+	local pMainPlot = Map.GetPlot(iX, iY)
+
+	if not (pMainPlot:GetOwner() ~= nil and Players[pMainPlot:GetOwner()]:GetCivilizationType() == GameInfoTypes.CIVILIZATION_ROME) then return end
+
+	local tPossibleSpots = {}
+								
+	for iDirection = 0, DirectionTypes.NUM_DIRECTION_TYPES - 1, 1 do	
+		local pPlot = Map.PlotDirection(iX, iY, iDirection)
+	
+		if pPlot:GetResourceType(-1) == (-1) and not pPlot:IsCity() and not pPlot:IsMountain() then		
+			if (pPlot:GetTerrainType() == TerrainTypes.TERRAIN_GRASS or pPlot:GetTerrainType() == TerrainTypes.TERRAIN_PLAINS or pPlot:GetTerrainType() == TerrainTypes.TERRAIN_HILLS or pPlot:GetTerrainType() == TerrainTypes.TERRAIN_DESERT) then
+				if pPlot:GetFeatureType() == (-1) or pPlot:GetFeatureType() == FeatureTypes.FEATURE_FOREST or pPlot:GetFeatureType() == FeatureTypes.FEATURE_JUNGLE or pPlot:GetFeatureType() == FeatureTypes.FEATURE_FLOOD_PLAINS or pPlot:GetFeatureType() == FeatureTypes.FEATURE_MARSH then
+					tPossibleSpots[#tPossibleSpots + 1] = pPlot
+				end
+			end
+		end
+	end
+	
+	local pNearestCity
+
+	if #tPossibleSpots <= 0 then
+		pNearestCity = GetNearestCity(pPlayer, pMainPlot)
+
+		if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+			pPlayer:AddNotification(0, 
+			'In [COLOR_CYAN]'..pNearestCity:GetName()..'[ENDCOLOR] there was not enough space for Latifundium to work out new source of [ICON_RES_FIGS] Figs.', 
+			'Cannot place source of Figs in '..pNearestCity:GetName(), 
+			iX, iY)
+		end	
+		
+		return 
+	end
+
+	local iChosenPlot = Game.Rand(#tPossibleSpots - 1, "") + 1
+	local pChosenPlot = tPossibleSpots[iChosenPlot]
+	local pCheckedPlayer = Players[pChosenPlot:GetOwner()]
+	local pCityThatLostTile = pChosenPlot:GetWorkingCity()
+	pNearestCity = GetNearestCity(pPlayer, pChosenPlot)
+
+	pChosenPlot:SetResourceType(GameInfoTypes.RESOURCE_ROME_FIGS, 1)
+	pChosenPlot:SetImprovementType(GameInfoTypes.IMPROVEMENT_PLANTATION)	
+	
+	if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+		pPlayer:AddNotification(NotificationTypes.NOTIFICATION_DISCOVERED_BONUS_RESOURCE, 
+		'In [COLOR_CYAN]'..pNearestCity:GetName()..'[ENDCOLOR] Latifundium worked out new source of [ICON_RES_FIGS] Figs.', 
+		'New source of Figs in '..pNearestCity:GetName(), 
+		pChosenPlot:GetX(), pChosenPlot:GetY(), GameInfoTypes.RESOURCE_ROME_FIGS)
+	end	
+			
+	if pCheckedPlayer == nil then
+		pChosenPlot:SetOwner(iPlayer, pNearestCity:GetID(), true, true)	
+
+		if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+			pPlayer:AddNotification(NotificationTypes.NOTIFICATION_CITY_TILE, 
+			'The City of '..pNearestCity:GetName()..'[ENDCOLOR] claimed new tile.', 
+			'Borders of '..pNearestCity:GetName()..' have grown', 
+			pChosenPlot:GetX(), pChosenPlot:GetY(), GameInfoTypes.RESOURCE_ROME_FIGS)
+		end
+	elseif pCheckedPlayer:GetCivilizationType() ~= GameInfoTypes.CIVILIZATION_ROME then
+		pChosenPlot:SetOwner(iPlayer, pNearestCity:GetID(), true, true)	
+
+		if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+			pPlayer:AddNotification(NotificationTypes.NOTIFICATION_CITY_TILE, 
+			'The City of '..pNearestCity:GetName()..'[ENDCOLOR] took a tile from another Empire!', 
+			'Borders of '..pNearestCity:GetName()..' have grown', 
+			pChosenPlot:GetX(), pChosenPlot:GetY(), GameInfoTypes.RESOURCE_ROME_FIGS)
+		end
+
+		if pCheckedPlayer:IsHuman() then
+			pCheckedPlayer:AddNotification(NotificationTypes.NOTIFICATION_CITY_TILE, 
+			'Roman Empire found a plantation in one of your Cities. '..pCityThatLostTile:GetName()..' lost a tile.', 
+			'The City of '..pCityThatLostTile:GetName()..' lost a tile!', 
+			pChosenPlot:GetX(), pChosenPlot:GetY())
+		end
+	end
+
+	if pChosenPlot:GetFeatureType() == FeatureTypes.FEATURE_FOREST then
+		pChosenPlot:SetFeatureType(FeatureTypes.NO_FEATURE, -1)
+		
+		local iClearing = math.floor(30 * fGameSpeedModifier)
+		pNearestCity:ChangeProduction(iClearing)
+									
+		if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+			Events.GameplayAlertMessage(Locale.ConvertTextKey("Clearing a Forest has created "..iClearing.." [ICON_PRODUCTION] Production for "..pNearestCity:GetName(), iClearing, pNearestCity:GetName()))
+		end
+	elseif pChosenPlot:GetFeatureType() == FeatureTypes.FEATURE_JUNGLE then
+		pChosenPlot:SetFeatureType(FeatureTypes.NO_FEATURE, -1)
+		
+		local iClearing = math.floor(20 * fGameSpeedModifier)
+		pNearestCity:ChangeProduction(iClearing)
+
+		if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+			Events.GameplayAlertMessage(Locale.ConvertTextKey("Clearing a Jungle has created "..iClearing.." [ICON_PRODUCTION] Production for "..pNearestCity:GetName(), iClearing, pNearestCity:GetName()))
+		end
+	elseif pChosenPlot:GetFeatureType() == FeatureTypes.FEATURE_MARSH then
+		pChosenPlot:SetFeatureType(FeatureTypes.NO_FEATURE, -1)
+	end
+end
+
+function GetNearestCity(pPlayer, pPlot)
+	local iDistance = 10000
+	local pNearestCity = nil
+
+	for pCity in pPlayer:Cities() do
+		local iDistanceToCity = Map.PlotDistance(pCity:GetX(), pCity:GetY(), pPlot:GetX(), pPlot:GetY())
+
+		if(iDistanceToCity < iDistance) then
+			iDistance = iDistanceToCity
+			pNearestCity = pCity
+		end
+	end
+
+	return pNearestCity
+end
+
+GameEvents.TileImprovementChanged.Add(LatifundiumSpawnFigs)
