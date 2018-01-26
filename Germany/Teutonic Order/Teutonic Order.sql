0ee66ae123961eade1bdeb0b53a37689b5e323a6
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-01-26 10:40:58 -0700

    Update Teutonic Order.sql

--- Germany/Teutonic Order/Teutonic Order.sql
+++ Germany/Teutonic Order/Teutonic Order.sql
@@ -8,6 +8,10 @@ INSERT INTO UnitPromotions
 	(Type,			Description,			Help,					CannotBeChosen,	Sound,			PortraitIndex,	IconAtlas,	 PediaType,	PediaEntry)
 VALUES	('PROMOTION_CHAPTER',	'TXT_KEY_PROMOTION_CHAPTER',	'TXT_KEY_PROMOTION_CHAPTER_HELP',	1,		'AS2D_IF_LEVELUP',	59,		'ABILITY_ATLAS', 'PEDIA_MELEE',	'TXT_KEY_PROMOTION_CHAPTER');
 
+INSERT INTO UnitPromotions_UnitCombats
+	(PromotionType, UnitCombatType)
+VALUES
+	('PROMOTION_CHAPTER', 'UNITCOMBAT_MELEE'),
 --------------------------------	
 -- Civilization_UnitClassOverrides
 --------------------------------	
