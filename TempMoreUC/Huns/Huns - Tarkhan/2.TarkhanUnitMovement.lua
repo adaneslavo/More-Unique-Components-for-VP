82001562817f264a03039f6669668fffb09bd0ed
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-01-26 12:40:09 -0700

    Update 2.TarkhanUnitMovement.lua
    Added Bellum Alet promotion function

--- TempMoreUC/Huns/Huns - Tarkhan/2.TarkhanUnitMovement.lua
+++ TempMoreUC/Huns/Huns - Tarkhan/2.TarkhanUnitMovement.lua
@@ -1,22 +1,57 @@
 --------------------------------------------------------------------------------------------------------------------------
--- ALAN CAVALRY
+-- TARKHAN
 --------------------------------------------------------------------------------------------------------------------------
---JFD_Vandals_AlanCavalry_PlayerDoTurn
-local unitClassSettlerID		 = GameInfoTypes["UNITCLASS_SETTLER"],["UNITCLASS_WORKER"]
-local unitPromotionAlanCavalryID = GameInfoTypes["PROMOTION_JFD_ALAN_CAVALRY"]
-function JFD_Vandals_AlanCavalry_PlayerDoTurn(playerID)
+--4UC Tarkhan Migration promotion
+local unitClassCivilianID		 = GameInfoTypes["UNITCLASS_SETTLER"],["UNITCLASS_WORKER"]
+local unitPromotionMigration = GameInfoTypes["PROMOTION_MIGRATION"]
+function HunMigration_PlayerDoTurn(playerID)
 	local player = Players[playerID]
 	if (not player:IsAlive()) then return end
-	if (not HasTrait(player, traitVandalsID)) then return end
 	for unit in player:Units() do
-		if unit:GetUnitClassType() == unitClassSettlerID then
+		if unit:GetUnitClassType() == unitClassCivilianID then
 			local plot = unit:GetPlot()
-			if (plot:GetUnit() and plot:GetUnit():IsHasPromotion(unitPromotionAlanCavalryID)) then
+			if (plot:GetUnit() and plot:GetUnit():IsHasPromotion(unitPromotionMigration)) then
 				unit:ChangeMoves(120)
 			end
 		end
 	end
 end
-GameEvents.PlayerDoTurn.Add(JFD_Vandals_AlanCavalry_PlayerDoTurn)
+GameEvents.PlayerDoTurn.Add(HunMigration_PlayerDoTurn)
 --==========================================================================================================================
 --==========================================================================================================================
+--Bellum Alet' promotion
+--------------------------------------------------------------
+include("PlotIterators.lua")
+include("FLuaVector.lua")
+include("InstanceManager")
+
+function BellumAlet(iPlayer, iUnit, iImprovement, iGold)
+	local pPlayer = Players[iPlayer]
+	local pUnit = pPlayer:GetUnitByID(iUnit)
+	
+	if pUnit and pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_SACKER) then
+		local unitPlot = pUnit:GetPlot()
+		
+		for iPlot in PlotRingIterator(unitPlot, 1, SECTOR_NORTH, DIRECTION_CLOCKWISE) do
+			for k = 0, iPlot:GetNumUnits() - 1 do
+				local jUnit = iPlot:GetUnit(k)
+				local iTeam = pPlayer:GetTeam()
+				
+					if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+						local vUnitPosition = PositionCalculator(jUnit:GetX(), jUnit:GetY())
+						
+						Events.AddPopupTextEvent(vUnitPosition, "[COLOR_GREEN]Bellum Alet[ENDCOLOR]", 1)
+					end
+
+					jUnit:ChangeDamage(-10, pPlayer)
+				end
+			end
+		end
+	end
+end
+
+function PositionCalculator(i1, i2)
+    return HexToWorld(ToHexFromGrid(Vector2(i1, i2)))
+end
+
+GameEvents.UnitPillageGold.Add(BellumAlet)
