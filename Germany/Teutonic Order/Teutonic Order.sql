dfa242411f7a3f844ea42766de54f603242f062d
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-01-25 20:20:26 -0700

    Update Teutonic Order.sql
    updated yields and added Chapter dummy promotion

--- Germany/Teutonic Order/Teutonic Order.sql
+++ Germany/Teutonic Order/Teutonic Order.sql
@@ -2,6 +2,13 @@
 -- Code by Blue Ghost
 
 --------------------------------	
+-- UnitPromotions
+--------------------------------	
+INSERT INTO UnitPromotions
+	(Type,			Description,			Help,					CannotBeChosen,	Sound,			PortraitIndex,	IconAtlas,	 PediaType,	PediaEntry)
+VALUES	('PROMOTION_CHAPTER',	'TXT_KEY_PROMOTION_CHAPTER',	'TXT_KEY_PROMOTION_CHAPTER_HELP',	1,		'AS2D_IF_LEVELUP',	59,		'ABILITY_ATLAS', 'PEDIA_MELEE',	'TXT_KEY_PROMOTION_CHAPTER');
+
+--------------------------------	
 -- Civilization_UnitClassOverrides
 --------------------------------	
 INSERT INTO	Civilization_BuildingClassOverrides
@@ -14,18 +21,23 @@ VALUES		('CIVILIZATION_GERMANY', 'BUILDINGCLASS_HEROIC_EPIC', 'BUILDING_GERMAN_T
 INSERT INTO	Buildings
 			(Type, Description, Civilopedia, Strategy, Help, GoldMaintenance, Cost, HurryCostModifier, MinAreaSize, ConquestProb,
 			BuildingClass, ArtDefineTag, PrereqTech, PortraitIndex, IconAtlas, GreatWorkYieldType,
-			NeverCapture, NukeImmune, NumCityCostMod, TrainedFreePromotion, GreatWorkSlotType, GreatWorkCount, NationalPopRequired, CitySupplyModifier)
+			NeverCapture, NukeImmune, NumCityCostMod, TrainedFreePromotion, FreePromotion, GreatWorkSlotType, GreatWorkCount, NationalPopRequired, CitySupplyModifier)
 SELECT		'BUILDING_GERMAN_TEUTONIC_ORDER', 'TXT_KEY_BUILDING_GERMAN_TEUTONIC_ORDER', 'TXT_KEY_BUILDING_GERMAN_TEUTONIC_ORDER_TEXT', 'TXT_KEY_BUILDING_GERMAN_TEUTONIC_ORDER_STRATEGY', 'TXT_KEY_BUILDING_GERMAN_TEUTONIC_ORDER_HELP', GoldMaintenance, Cost, HurryCostModifier, MinAreaSize, ConquestProb,
 			BuildingClass, ArtDefineTag, PrereqTech, 3, 'BUILDING_APIG4UC_ATLAS', GreatWorkYieldType,
-			NeverCapture, NukeImmune, NumCityCostMod, TrainedFreePromotion, GreatWorkSlotType, GreatWorkCount, NationalPopRequired, CitySupplyModifier
+			NeverCapture, NukeImmune, NumCityCostMod, TrainedFreePromotion, FreePromotion, GreatWorkSlotType, GreatWorkCount, NationalPopRequired, CitySupplyModifier
 FROM		Buildings
 WHERE		Type = 'BUILDING_HEROIC_EPIC';
 
 UPDATE		Buildings
-SET			PrereqTech = 'TECH_CHIVALRY', ExtraCityHitPoints = 100, FreeBuildingThisCity = 'BUILDINGCLASS_ARMORY'
+SET			PrereqTech = 'TECH_CHIVALRY', ExtraCityHitPoints = 50, FreeBuildingThisCity = 'BUILDINGCLASS_ARMORY', FreePromotion = 'PROMOTION_CHAPTER'
 WHERE		Type = 'BUILDING_GERMAN_TEUTONIC_ORDER';
 
-
+------------------------------	
+-- Building_ClassesNeededInCity
+------------------------------		
+INSERT INTO Building_ClassesNeededInCity 	
+		(BuildingType,				BuildingClassType)
+VALUES		('BUILDING_GERMAN_TEUTONIC_ORDER',	'BUILDINGCLASS_BARRACKS');
 
 --------------------------------	
 -- Building_BuildingClassYieldChanges
@@ -40,12 +52,12 @@ VALUES			('BUILDING_GERMAN_TEUTONIC_ORDER', 'BUILDINGCLASS_BARRACKS', 'YIELD_FAI
 --------------------------------	
 INSERT INTO	Building_Flavors
 			(BuildingType, FlavorType, Flavor)
-VALUES		('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_RELIGION', 10),
+VALUES		('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_RELIGION', 30),
 			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_OFFENSE', 15),
 			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_DEFENSE', 10),
 			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_MILITARY_TRAINING', 50),
-			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_PRODUCTION', 5),
-			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_CULTURE', 10),
+			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_PRODUCTION', 15),
+			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_CULTURE', 5),
 			('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_WONDER', 20);
 
 
@@ -54,12 +66,19 @@ VALUES		('BUILDING_GERMAN_TEUTONIC_ORDER', 'FLAVOR_RELIGION', 10),
 --------------------------------	
 INSERT INTO Building_YieldChanges
 			(BuildingType, YieldType, Yield)
-VALUES		('BUILDING_GERMAN_TEUTONIC_ORDER', 'YIELD_CULTURE', 3),
-			('BUILDING_GERMAN_TEUTONIC_ORDER', 'YIELD_SCIENCE', 3);
+VALUES		('BUILDING_GERMAN_TEUTONIC_ORDER', 'YIELD_CULTURE', 1),
+			('BUILDING_GERMAN_TEUTONIC_ORDER', 'YIELD_FAITH', 3);
+
+INSERT INTO Building_YieldChangesPerPop 	
+	(BuildingType,				YieldType,		Yield)
+VALUES	'BUILDING_GERMAN_TEUTONIC_ORDER',	'YIELD_PRODUCTION', 	40);
 
 
+--------------------------------	
+-- Dummy_Building
+--------------------------------	
 INSERT INTO BuildingClasses
-			(Type,										DefaultBuilding)
+		(Type,					DefaultBuilding)
 VALUES		('BUILDINGCLASS_TEUTONIC_ORDER_DUMMY',	'BUILDING_TEUTONIC_ORDER_DUMMY');
 
 INSERT INTO Buildings
