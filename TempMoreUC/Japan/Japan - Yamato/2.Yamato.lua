d2944b44ba8df405683f2ffd8ce395bab57447b1
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-02-02 17:26:37 -0700

    Create 2.Yamato.lua
    Yamato lua for yields on unit creation and yields for killing a yamato.
    
    Please check through; not sure if I did this right

---
+++ TempMoreUC/Japan/Japan - Yamato/2.Yamato.lua
@@ -0,0 +1,31 @@
+-- Yamato
+-- Author: pineappledan
+-- DateCreated: 02/02/2018
+--------------------------------------------------------------
+
+--GAP on construction of Yamato
+
+function YamatoOnBuild(iPlayer, iCity, iUnit)
+	local pPlayer = Players[iPlayer]
+	local pUnit = pPlayer:GetUnitByID(iUnit)
+
+	if pUnit:GetUnitType() == UNIT_YAMATO then
+		pPlayer:ChangeGoldenAgeProgressMeter(600)
+
+	end
+end
+
+--GAP and Culture on killing Yamato
+
+function YamatoOnDeath(iPlayerKiller, iPlayerKillee, KillerUnitType, KilledUnitType)
+	local pPlayerKiller = Players[iPlayerKiller]
+
+	if KilledUnitType:GetUnitType() == UNIT_YAMATO then
+		pPlayerKiller:ChangeJONSCulture(400)
+		pPlayerKiller:ChangeGoldenAgeProgressMeter(400)
+	
+	end		
+end
+			
+GameEvents.CityTrained.Add(YamatoOnBuild)
+GameEvents.UnitKilledInCombat.Add(YamatoOnDeath)
