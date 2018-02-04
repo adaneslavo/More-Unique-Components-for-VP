07113eac26417fea8d61c48c51f8f22058356697
adan_eslavo <adan_eslavo@o2.pl>
2018-02-04 19:55:11 +0100

    v25.15
    Fixed Sofa. Changed folder names. Finished Move/Build/Create rework. Chasqui rework.

--- Songhai/Sofa/2.Sofa.lua
+++ Songhai/Sofa/2.Sofa.lua
@@ -1,72 +1,19 @@
 -- HorseDad
--- Author: pineappledan (modified from Goedendag by Adan_eslavo)
+-- Author: pineappledan, adan_eslavo
 -- DateCreated: 02/02/2018
 -- 2018-02-04 Modified by Infixo
 --------------------------------------------------------------
 local iSofa = GameInfoTypes.PROMOTION_HORSE_FATHER
 local iHorseDadEffect = GameInfoTypes.PROMOTION_HORSE_FATHER_EFFECT
 
-function HorseDadOnMove(iPlayer, iUnit, iX, iY)
-	local pPlayer = Players[iPlayer];
+function HorseDaddy(iPlayer)
+	local pPlayer = Players[iPlayer]
 
 	for pUnit in pPlayer:Units() do
-		if pUnit:IsMounted() then
-			--[[
-			local bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 0, true, false)
-
-			if not bInRange then
-				bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, true, false)
-				
-				if not bInRange then
-					bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 2, true, false)
-				end
-			end
-			--]]
+		if GameInfo.Units[pUnit:GetUnitType()].IsMounted then
 			pUnit:SetHasPromotion(iHorseDadEffect, pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 2, true, false))
 		end
 	end
 end
 
-function HorseDadOnCreate(iPlayer, iUnit, iUnitType, iX, iY)
-	local pPlayer = Players[iPlayer]
-	local pUnit = pPlayer:GetUnitByID(iUnit)
-
-	if pUnit:IsMounted() then
-		--[[
-		local bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 0, true, false)
-
-		if not bInRange then
-			bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, true, false)
-				
-			if not bInRange then
-				bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 2, true, false)
-			end
-		end
-		--]]
-		pUnit:SetHasPromotion(iHorseDadEffect, pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 2, true, false))
-	end
-end
-
-function HorseDadOnBuild(iPlayer, iCity, iUnit)
-	local pPlayer = Players[iPlayer]
-	local pUnit = pPlayer:GetUnitByID(iUnit)
-
-	if pUnit:IsMounted() then
-		--[[
-		local bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 0, true, false)
-
-		if not bInRange then
-			bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, true, false)
-				
-			if not bInRange then
-				bInRange = pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 2, true, false)
-			end
-		end
-		--]]
-		pUnit:SetHasPromotion(iHorseDadEffect, pUnit:IsWithinDistanceOfUnitPromotion(iSofa, 2, true, false))
-	end
-end
-
-GameEvents.UnitSetXY.Add(HorseDadOnMove)
-GameEvents.UnitCreated.Add(HorseDadOnCreate)
-GameEvents.CityTrained.Add(HorseDadOnBuild)
+GameEvents.PlayerEndTurnCompleted.Add(HorseDaddy)
