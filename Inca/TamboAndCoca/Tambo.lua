43054c21bdbf53f478f426a213b2daaa173c65cf
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-02-12 20:29:46 -0700

    Update Tambo.lua
    Added TamboPlaceCoca and CocaRemoved functions

--- Inca/TamboAndCoca/Tambo.lua
+++ Inca/TamboAndCoca/Tambo.lua
@@ -1,9 +1,47 @@
 --------------------------------------------------------------
 -- Inca - Tambo
--- Coca Monopoly gives promotion to eligible military and civilian units
--- Author: Infixo
+-- Author: Infixo, pineappledan
 -- DateCreated: 2018-02-03
 --------------------------------------------------------------
+-- Coca Placement underneath city
+function TamboPlaceCoca(iPlayer, iCity, iBuilding)
+	--print("BuffaloPoundPlaceBison",iPlayer, iCity, iBuilding)
+	-- check for Inca
+	local pPlayer = Players[iPlayer]
+	if not( pPlayer and pPlayer:GetCivilizationType() == GameInfoTypes.CIVILIZATION_INCA ) then return end
+	-- check for Buffalo Pound
+	if iBuilding ~= GameInfoTypes.BUILDING_TAMBO then return end
+	local pCity = pPlayer:GetCityByID(iCity)
+	if not pCity then return end
+	-- If no resource under city then spawn Coca
+	if iCityPlot:GetNumResource() == 0 then
+		iCityPlot:SetResourceType(GameInfoTypes.RESOURCE_COCA, 1)
+		bCocaCreated = true
+	end
+
+	-- send a proper notification
+	if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+		if bCocaCreated then 
+			pPlayer:AddNotification(
+				NotificationTypes.NOTIFICATION_DISCOVERED_LUXURY_RESOURCE,
+				'The City of [COLOR_CYAN]'..pCity:GetName()..'[ENDCOLOR] constructed a Tambo and [COLOR_POSITIVE_TEXT]created[ENDCOLOR] a new source of [ICON_RES_COCA] Coca.', -- use TXT_KEY_ here
+				'New source of Coca in '..pCity:GetName()..' [COLOR_POSITIVE_TEXT]created[ENDCOLOR]',
+				iCityPlot:GetX(), iCityPlot:GetY(), GameInfoTypes.RESOURCE_COCA)
+		end
+	end
+end
+
+-- Coca resource is removed when a city is razed
+
+function CocaRemoved(iHexPos, iPlayer1, iCity, iPlayer2)
+	local pPlayer = Players[iPlayer1]
+	local pCity = pPlayer:GetCityByID(iCity)
+	for iCityPlot do		
+		iCityPlot:SetResourceType(GameInfoTypes.RESOURCE_COCA, 0)
+	end			
+end			
+-- Coca Monopoly gives promotion to eligible military and civilian units
+
 local ePromotionMilitary = GameInfoTypes.PROMOTION_COCA_MILITARY
 local ePromotionCivilian = GameInfoTypes.PROMOTION_COCA_WORKER
 
@@ -32,4 +70,7 @@ function OnPlayerDoTurnTambo(iPlayer)
 		if tEligibleClasses[ unit:GetUnitClassType() ] then unit:SetHasPromotion(ePromotionCivilian, bHasCocaMonopoly) end
 	end
 end
+
 GameEvents.PlayerDoTurn.Add(OnPlayerDoTurnTambo)
+GameEvents.CityConstructed.Add(TamboPlaceCoca)
+Events.SerialEventCityDestroyed.Add(CocaRemoved)
