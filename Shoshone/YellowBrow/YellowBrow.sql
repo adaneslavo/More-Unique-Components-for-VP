da2a55c041508ac5593e75ffbf4da6431b89eee5
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-02-15 15:27:27 -0700

    Update YellowBrow.sql
    replaced unit default art defines, from Tercio to Zulu Impi

--- Shoshone/YellowBrow/YellowBrow.sql
+++ Shoshone/YellowBrow/YellowBrow.sql
@@ -24,35 +24,35 @@ VALUES		('ART_DEF_UNIT_SHOSHONE_YELLOW_BROW',	'Unit',		'YellowBrowFlag_128.dds')
 INSERT INTO ArtDefine_UnitInfos 
 			(Type,									DamageStates,	Formation)
 SELECT		'ART_DEF_UNIT_SHOSHONE_YELLOW_BROW',	DamageStates, 	Formation
-FROM ArtDefine_UnitInfos WHERE Type = 'ART_DEF_UNIT_U_SPANISH_TERCIO';
+FROM ArtDefine_UnitInfos WHERE Type = 'ART_DEF_UNIT_U_ZULU_IMPI_WARRIOR';
 ------------------------------
 -- ArtDefine_UnitInfoMemberInfos
 ------------------------------
 INSERT INTO ArtDefine_UnitInfoMemberInfos 	
-			(UnitInfoType,							UnitMemberInfoType,								NumMembers)
+			(UnitInfoType,			UnitMemberInfoType,				NumMembers)
 SELECT		'ART_DEF_UNIT_SHOSHONE_YELLOW_BROW',	'ART_DEF_UNIT_MEMBER_SHOSHONE_YELLOW_BROW', 	NumMembers
-FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType = 'ART_DEF_UNIT_U_SPANISH_TERCIO';
+FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType = 'ART_DEF_UNIT_U_ZULU_IMPI_WARRIOR';
 ------------------------------
 -- ArtDefine_UnitMemberCombats
 ------------------------------
 INSERT INTO ArtDefine_UnitMemberCombats 
-			(UnitMemberType,							EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation)
+			(UnitMemberType,			EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation)
 SELECT		'ART_DEF_UNIT_MEMBER_SHOSHONE_YELLOW_BROW',	EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation
-FROM ArtDefine_UnitMemberCombats WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_U_SPANISH_TERCIO_PIQUERO';
+FROM ArtDefine_UnitMemberCombats WHERE UnitMemberType = 'ART_DEF_UNIT_U_ZULU_IMPI_WARRIOR';
 ------------------------------
 -- ArtDefine_UnitMemberCombatWeapons
 ------------------------------
 INSERT INTO ArtDefine_UnitMemberCombatWeapons	
 			(UnitMemberType,							"Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag)
 SELECT		'ART_DEF_UNIT_MEMBER_SHOSHONE_YELLOW_BROW',	"Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag
-FROM ArtDefine_UnitMemberCombatWeapons WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_U_SPANISH_TERCIO_PIQUERO';
+FROM ArtDefine_UnitMemberCombatWeapons WHERE UnitMemberType = 'ART_DEF_UNIT_U_ZULU_IMPI_WARRIOR';
 ------------------------------
 -- ArtDefine_UnitMemberInfos
 ------------------------------
 INSERT INTO ArtDefine_UnitMemberInfos 	
-		(Type, 										Scale, ZOffset, Domain, Model, 					MaterialTypeTag, MaterialTypeSoundOverrideTag)
+		(Type, 					Scale, ZOffset, Domain, Model, 			MaterialTypeTag, MaterialTypeSoundOverrideTag)
 SELECT	'ART_DEF_UNIT_MEMBER_SHOSHONE_YELLOW_BROW',	Scale, ZOffset, Domain, 'MC_BlackMouth.fxsxml',	MaterialTypeTag, MaterialTypeSoundOverrideTag
-FROM ArtDefine_UnitMemberInfos WHERE Type = 'ART_DEF_UNIT_MEMBER_U_SPANISH_TERCIO_PIQUERO';
+FROM ArtDefine_UnitMemberInfos WHERE Type = 'ART_DEF_UNIT_U_ZULU_IMPI_WARRIOR';
 ------------------------------
 -- IconTextureAtlases
 ------------------------------
