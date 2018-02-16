2c8c83fc3c2acdeb870c258ffde4a4aea5eea953
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-02-15 19:40:45 -0700

    Update 1.Yamato.sql
    'cleaned' up yamato (no changes)

--- Japan/Yamato/1.Yamato.sql
+++ Japan/Yamato/1.Yamato.sql
@@ -22,48 +22,48 @@ VALUES		('ART_DEF_UNIT_YAMATO',	'Unit',		'sv_Yamato.dds');
 -- ArtDefine_UnitInfos
 ------------------------------		
 INSERT INTO ArtDefine_UnitInfos 
-			(Type,							DamageStates,	Formation)
+			(Type,		DamageStates,	Formation)
 SELECT		'ART_DEF_UNIT_YAMATO',	DamageStates, 	Formation
 FROM ArtDefine_UnitInfos WHERE Type = 'ART_DEF_UNIT_BATTLESHIP';
 ------------------------------
 -- ArtDefine_UnitInfoMemberInfos
 ------------------------------
 INSERT INTO ArtDefine_UnitInfoMemberInfos 	
-			(UnitInfoType,					UnitMemberInfoType,						NumMembers)
-SELECT		'ART_DEF_UNIT_YAMATO',	'ART_DEF_UNIT_MEMBER_YAMATO', 	NumMembers
+	(UnitInfoType,		UnitMemberInfoType,		NumMembers)
+SELECT	'ART_DEF_UNIT_YAMATO',	'ART_DEF_UNIT_MEMBER_YAMATO', 	NumMembers
 FROM ArtDefine_UnitInfoMemberInfos WHERE UnitInfoType = 'ART_DEF_UNIT_BATTLESHIP';
 ------------------------------
 -- ArtDefine_UnitMemberCombats
 ------------------------------
 INSERT INTO ArtDefine_UnitMemberCombats 
-			(UnitMemberType,					EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation)
-SELECT		'ART_DEF_UNIT_MEMBER_YAMATO',	EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation
+	(UnitMemberType,		EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation)
+SELECT	'ART_DEF_UNIT_MEMBER_YAMATO',	EnableActions, DisableActions, MoveRadius, ShortMoveRadius, ChargeRadius, AttackRadius, RangedAttackRadius, MoveRate, ShortMoveRate, TurnRateMin, TurnRateMax, TurnFacingRateMin, TurnFacingRateMax, RollRateMin, RollRateMax, PitchRateMin, PitchRateMax, LOSRadiusScale, TargetRadius, TargetHeight, HasShortRangedAttack, HasLongRangedAttack, HasLeftRightAttack, HasStationaryMelee, HasStationaryRangedAttack, HasRefaceAfterCombat, ReformBeforeCombat, HasIndependentWeaponFacing, HasOpponentTracking, HasCollisionAttack, AttackAltitude, AltitudeDecelerationDistance, OnlyTurnInMovementActions, RushAttackFormation
 FROM ArtDefine_UnitMemberCombats WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_BATTLESHIP';
 ------------------------------
 -- ArtDefine_UnitMemberCombatWeapons
 ------------------------------
 INSERT INTO ArtDefine_UnitMemberCombatWeapons	
-			(UnitMemberType,					"Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag)
+		(UnitMemberType,		"Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag)
 SELECT		'ART_DEF_UNIT_MEMBER_YAMATO',	"Index", SubIndex, ID, VisKillStrengthMin, VisKillStrengthMax, ProjectileSpeed, ProjectileTurnRateMin, ProjectileTurnRateMax, HitEffect, HitEffectScale, HitRadius, ProjectileChildEffectScale, AreaDamageDelay, ContinuousFire, WaitForEffectCompletion, TargetGround, IsDropped, WeaponTypeTag, WeaponTypeSoundOverrideTag
 FROM ArtDefine_UnitMemberCombatWeapons WHERE UnitMemberType = 'ART_DEF_UNIT_MEMBER_BATTLESHIP';
 ------------------------------
 -- ArtDefine_UnitMemberInfos
 ------------------------------
 INSERT INTO ArtDefine_UnitMemberInfos 	
-		(Type, 								Scale,	ZOffset, Domain, Model, 							MaterialTypeTag, MaterialTypeSoundOverrideTag)
+	(Type, 				Scale,	ZOffset, Domain, Model, 		MaterialTypeTag, MaterialTypeSoundOverrideTag)
 SELECT	'ART_DEF_UNIT_MEMBER_YAMATO',	Scale,	ZOffset, Domain, 'Yamato_Class.fxsxml',	MaterialTypeTag, MaterialTypeSoundOverrideTag
 FROM ArtDefine_UnitMemberInfos WHERE Type = 'ART_DEF_UNIT_MEMBER_BATTLESHIP';
 ------------------------------
 -- IconTextureAtlases
 ------------------------------
 INSERT INTO IconTextureAtlases 
-			(Atlas, 					IconSize, 	Filename, 					IconsPerRow, 	IconsPerColumn)
-VALUES		('UNIT_YAMATO_ATLAS', 		256, 		'JFD_JapanTojoAtlas_256.dds',	1, 				1),
-			('UNIT_YAMATO_ATLAS', 		128, 		'JFD_JapanTojoAtlas_128.dds',	1, 				1),
-			('UNIT_YAMATO_ATLAS', 		80, 		'JFD_JapanTojoAtlas_80.dds',	1, 				1),
-			('UNIT_YAMATO_ATLAS', 		64, 		'JFD_JapanTojoAtlas_64.dds',	1, 				1),
-			('UNIT_YAMATO_ATLAS', 		45, 		'JFD_JapanTojoAtlas_45.dds',	1, 				1),
-			('UNIT_YAMATO_FLAG_ATLAS', 	32, 		'UnitFlagYamatoAtlas_32.dds',		1, 				1);
+	(Atlas, 			IconSize, 	Filename, 			IconsPerRow, 	IconsPerColumn)
+VALUES	('UNIT_YAMATO_ATLAS', 		256, 		'JFD_JapanTojoAtlas_256.dds',	1, 		1),
+	('UNIT_YAMATO_ATLAS', 		128, 		'JFD_JapanTojoAtlas_128.dds',	1, 		1),
+	('UNIT_YAMATO_ATLAS', 		80, 		'JFD_JapanTojoAtlas_80.dds',	1, 		1),
+	('UNIT_YAMATO_ATLAS', 		64, 		'JFD_JapanTojoAtlas_64.dds',	1, 		1),
+	('UNIT_YAMATO_ATLAS', 		45, 		'JFD_JapanTojoAtlas_45.dds',	1, 		1),
+	('UNIT_YAMATO_FLAG_ATLAS', 	32, 		'UnitFlagYamatoAtlas_32.dds',	1, 		1);
 --==========================================================================================================================	
 
 --==========================================================================================================================	
@@ -75,44 +75,44 @@ VALUES		('UNIT_YAMATO_ATLAS', 		256, 		'JFD_JapanTojoAtlas_256.dds',	1, 				1),
 -- Units
 --------------------------------	
 INSERT INTO Units 	
-			(Type,					Class, 	PrereqTech,			Range, BaseLandAirDefense,	BaseSightRange, Combat, RangedCombat,		Cost,		FaithCost,	RequiresFaithPurchaseEnabled,	Moves,		Immobile, CombatClass, Domain, DefaultUnitAI,	ObsoleteTech,		GoodyHutUpgradeUnitClass,	XPValueAttack,	Description, 					Civilopedia, 						Strategy, 								Help, 								Pillage, MilitarySupport, MilitaryProduction, IgnoreBuildingDefense, Mechanized, AirUnitCap, AdvancedStartCost, RangedCombatLimit, CombatLimit, XPValueDefense, UnitArtInfo, 					UnitFlagIconOffset,	UnitFlagAtlas,				PortraitIndex, 	IconAtlas,			MoveRate)
-SELECT		'UNIT_YAMATO',	Class,	'TECH_ROCKETRY',	Range+1,	BaseLandAirDefense, BaseSightRange, Combat+5, RangedCombat+15,	Cost+200,	FaithCost,	RequiresFaithPurchaseEnabled,	Moves,	Immobile, CombatClass, Domain, DefaultUnitAI,	ObsoleteTech,	GoodyHutUpgradeUnitClass,	XPValueAttack,	'TXT_KEY_UNIT_YAMATO',	'TXT_KEY_UNIT_YAMATO_TEXT',	'TXT_KEY_UNIT_YAMATO_STRATEGY',	'TXT_KEY_UNIT_YAMATO_HELP',	Pillage, MilitarySupport, MilitaryProduction, IgnoreBuildingDefense, Mechanized, AirUnitCap, AdvancedStartCost, RangedCombatLimit, CombatLimit, XPValueDefense, 'ART_DEF_UNIT_YAMATO', 	0,					'UNIT_YAMATO_FLAG_ATLAS',	0, 				'UNIT_YAMATO_ATLAS',	MoveRate
+		(Type,		Class, 	PrereqTech,		Range, 		BaseLandAirDefense, BaseSightRange, Combat, 	RangedCombat,		Cost,		FaithCost,	RequiresFaithPurchaseEnabled,	Moves,	Immobile, CombatClass, Domain, DefaultUnitAI,	ObsoleteTech,	GoodyHutUpgradeUnitClass,	XPValueAttack,	Description, 		Civilopedia, 			Strategy, 			Help, 				Pillage, MilitarySupport, MilitaryProduction, IgnoreBuildingDefense, Mechanized, AirUnitCap, AdvancedStartCost, RangedCombatLimit, CombatLimit, XPValueDefense, UnitArtInfo, 		UnitFlagIconOffset,	UnitFlagAtlas,			PortraitIndex, 	IconAtlas,		MoveRate)
+SELECT		'UNIT_YAMATO',	Class,	'TECH_ROCKETRY',	Range+1,	BaseLandAirDefense, BaseSightRange, Combat+5, 	RangedCombat+15,	Cost+200,	FaithCost,	RequiresFaithPurchaseEnabled,	Moves,	Immobile, CombatClass, Domain, DefaultUnitAI,	ObsoleteTech,	GoodyHutUpgradeUnitClass,	XPValueAttack,	'TXT_KEY_UNIT_YAMATO',	'TXT_KEY_UNIT_YAMATO_TEXT',	'TXT_KEY_UNIT_YAMATO_STRATEGY',	'TXT_KEY_UNIT_YAMATO_HELP',	Pillage, MilitarySupport, MilitaryProduction, IgnoreBuildingDefense, Mechanized, AirUnitCap, AdvancedStartCost, RangedCombatLimit, CombatLimit, XPValueDefense, 'ART_DEF_UNIT_YAMATO', 	0,			'UNIT_YAMATO_FLAG_ATLAS',	0, 		'UNIT_YAMATO_ATLAS',	MoveRate
 FROM Units WHERE Type = 'UNIT_BATTLESHIP';
 --------------------------------	
 -- UnitGameplay2DScripts
 --------------------------------		
 INSERT INTO UnitGameplay2DScripts 	
-			(UnitType, 				SelectionSound, FirstSelectionSound)
-SELECT		'UNIT_YAMATO',	SelectionSound, FirstSelectionSound
+	(UnitType, 	SelectionSound, FirstSelectionSound)
+SELECT	'UNIT_YAMATO',	SelectionSound, FirstSelectionSound
 FROM UnitGameplay2DScripts WHERE UnitType = 'UNIT_BATTLESHIP';	
 --------------------------------	
 -- Unit_AITypes
 --------------------------------		
 INSERT INTO Unit_AITypes 	
-			(UnitType, 				UnitAIType)
-SELECT		'UNIT_YAMATO',	UnitAIType
+	(UnitType, 	UnitAIType)
+SELECT	'UNIT_YAMATO',	UnitAIType
 FROM Unit_AITypes WHERE UnitType = 'UNIT_BATTLESHIP';
 --------------------------------	
 -- Unit_ResourceQuantityRequirements
 --------------------------------		
 INSERT INTO Unit_ResourceQuantityRequirements 	
-			(UnitType, 				ResourceType)
+		(UnitType, 	ResourceType)
 SELECT		'UNIT_YAMATO',	ResourceType
 FROM Unit_ResourceQuantityRequirements WHERE UnitType = 'UNIT_BATTLESHIP';
 --------------------------------	
 -- Unit_ClassUpgrades
 --------------------------------		
 INSERT INTO Unit_ClassUpgrades 	
-			(UnitType, 				UnitClassType)
-SELECT		'UNIT_YAMATO',	UnitClassType
+	(UnitType, 	UnitClassType)
+SELECT	'UNIT_YAMATO',	UnitClassType
 FROM Unit_ClassUpgrades WHERE UnitType = 'UNIT_BATTLESHIP';
 --------------------------------	
 -- Unit_Flavors
 --------------------------------		
 INSERT INTO Unit_Flavors 	
-			(UnitType, 				FlavorType,				Flavor)
-VALUES		('UNIT_YAMATO',	'FLAVOR_NAVAL',			45),
-			('UNIT_YAMATO',	'FLAVOR_NAVAL_RECON',	5);
+	(UnitType, 	FlavorType,		Flavor)
+VALUES	('UNIT_YAMATO',	'FLAVOR_NAVAL',		45),
+	('UNIT_YAMATO',	'FLAVOR_NAVAL_RECON',	5);
 
 --==========================================================================================================================	
 -- PROMOTIONS
@@ -127,7 +127,7 @@ VALUES		('PROMOTION_UNIT_YAMATO', 	'TXT_KEY_PROMOTION_UNIT_YAMATO',	'TXT_KEY_PRO
 -- UnitPromotions_YieldFromKills
 ------------------------------
 INSERT INTO UnitPromotions_YieldFromKills
-			(PromotionType, 						YieldType,						Yield)
+			(PromotionType, 	YieldType,			Yield)
 VALUES		('PROMOTION_UNIT_YAMATO',	'YIELD_GREAT_ADMIRAL_POINTS',	400);
 --==========================================================================================================================	
 
