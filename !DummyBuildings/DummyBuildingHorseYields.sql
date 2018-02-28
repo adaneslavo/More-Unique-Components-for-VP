75d923b80819171a26045c5044429daa56650e40
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-02-28 11:52:55 -0700

    Update DummyBuildingHorseYields.sql

--- !DummyBuildings/DummyBuildingHorseYields.sql
+++ !DummyBuildings/DummyBuildingHorseYields.sql
@@ -5,7 +5,7 @@
 -- BuildingClasses
 ------------------------------
 INSERT INTO BuildingClasses
-(Type, DefaultBuilding, NoLimit)
+         (Type, DefaultBuilding, NoLimit)
 VALUES ('BUILDINGCLASS_HIPPODUMMY', 'BUILDING_HIPPO_DUMMY', 1);
 --==========================================================================================================================
 --==========================================================================================================================
@@ -15,13 +15,13 @@ VALUES ('BUILDINGCLASS_HIPPODUMMY', 'BUILDING_HIPPO_DUMMY', 1);
 -- Buildings
 ------------------------------
 INSERT INTO Buildings
-(Type, BuildingClass, Description, GoldMaintenance, Cost, FaithCost, GreatWorkCount, NeverCapture, NukeImmune, ConquestProb, HurryCostModifier, IconAtlas, PortraitIndex)
-VALUES ('BUILDING_HIPPO_DUMMY', 'BUILDINGCLASS_HIPPODUMMY', 'TXT_KEY_BUILDING_HIPPO_DUMMY', 0, -1, -1, -1, 1, 1, 0, -1, 'CIV_COLOR_ATLAS', 0);
+         (Type,                     BuildingClass,             Description,                  GoldMaintenance,  Cost, FaithCost, GreatWorkCount, NeverCapture,  NukeImmune, ConquestProb, HurryCostModifier, IconAtlas,        PortraitIndex)
+VALUES   ('BUILDING_HIPPO_DUMMY', 'BUILDINGCLASS_HIPPODUMMY', 'TXT_KEY_BUILDING_HIPPO_DUMMY', 0,               -1,   -1,         -1,            1,             1,          0,             -1,               'CIV_COLOR_ATLAS', 0);
 --==========================================================================================================================
 ------------------------------
 -- Building_ResourceYieldChanges
 ------------------------------
 INSERT INTO Building_ResourceYieldChanges
-   (BuildingType, ResourceType, YieldType, Yield)
-VALUES ('BUILDING_HIPPO_DUMMY', 'RESOURCE_HORSE', 'YIELD_CULTURE', 1),
-   ('BUILDING_HIPPO_DUMMY', 'RESOURCE_HORSE', 'YIELD_GOLD', 1);
+         (BuildingType,          ResourceType,     YieldType,     Yield)
+VALUES   ('BUILDING_HIPPO_DUMMY', 'RESOURCE_HORSE', 'YIELD_CULTURE', 1),
+         ('BUILDING_HIPPO_DUMMY', 'RESOURCE_HORSE', 'YIELD_GOLD', 1);
