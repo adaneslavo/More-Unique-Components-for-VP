fb50e1746e301f5a7ec040e83838733894766830
adan_eslavo <adan_eslavo@o2.pl>
2018-03-01 18:56:58 +0100

    v28.3
    Checked and fixed Hippodrome.

--- !DummyBuildings/DummyBuildingHorseYields.sql
+++ !DummyBuildings/DummyBuildingHorseYields.sql
@@ -5,9 +5,10 @@
 -- BuildingClasses
 ------------------------------
 INSERT INTO BuildingClasses
-         (Type, DefaultBuilding, NoLimit)
-VALUES ('BUILDINGCLASS_HIPPODUMMY', 'BUILDING_HIPPO_DUMMY', 1);
+			(Type,								DefaultBuilding,				NoLimit)
+VALUES		('BUILDINGCLASS_DUMMYHORSEYIELDS',	'BUILDING_DUMMYHORSEYIELDS',	1);
 --==========================================================================================================================
+
 --==========================================================================================================================
 -- BUILDINGS
 --==========================================================================================================================
@@ -15,13 +16,14 @@ VALUES ('BUILDINGCLASS_HIPPODUMMY', 'BUILDING_HIPPO_DUMMY', 1);
 -- Buildings
 ------------------------------
 INSERT INTO Buildings
-         (Type,                     BuildingClass,             Description,                  GoldMaintenance,  Cost, FaithCost, GreatWorkCount, NeverCapture,  NukeImmune, ConquestProb, HurryCostModifier, IconAtlas,        PortraitIndex)
-VALUES   ('BUILDING_HIPPO_DUMMY', 'BUILDINGCLASS_HIPPODUMMY', 'TXT_KEY_BUILDING_HIPPO_DUMMY', 0,               -1,   -1,         -1,            1,             1,          0,             -1,               'CIV_COLOR_ATLAS', 0);
---==========================================================================================================================
+			(Type,							BuildingClass,						Description,							GoldMaintenance,	Cost, FaithCost,	GreatWorkCount, NeverCapture,  NukeImmune, ConquestProb,	HurryCostModifier,	IconAtlas,			PortraitIndex)
+VALUES		('BUILDING_DUMMYHORSEYIELDS',	'BUILDINGCLASS_DUMMYHORSEYIELDS',	'TXT_KEY_BUILDING_DUMMYHORSEYIELDS',	0,					-1,   -1,			-1,				1,             1,          0,				-1,					'CIV_COLOR_ATLAS',	0);
 ------------------------------
 -- Building_ResourceYieldChanges
 ------------------------------
 INSERT INTO Building_ResourceYieldChanges
-         (BuildingType,          ResourceType,     YieldType,     Yield)
-VALUES   ('BUILDING_HIPPO_DUMMY', 'RESOURCE_HORSE', 'YIELD_CULTURE', 1),
-         ('BUILDING_HIPPO_DUMMY', 'RESOURCE_HORSE', 'YIELD_GOLD', 1);
+			(BuildingType,					ResourceType,     YieldType,			Yield)
+VALUES		('BUILDING_DUMMYHORSEYIELDS',	'RESOURCE_HORSE', 'YIELD_CULTURE',		1),
+			('BUILDING_DUMMYHORSEYIELDS',	'RESOURCE_HORSE', 'YIELD_GOLD',			1);
+--==========================================================================================================================
+--==========================================================================================================================
--- Byzantium/Hippodrome/Hippodrome.lua
+++ Byzantium/Hippodrome/Hippodrome.lua
@@ -4,72 +4,73 @@
 --------------------------------------------------------------
 include("FLuaVector.lua")
 
-function HippodromeBuilt(iPlayer, iCity, iBuilding)
+local eBuildingHippodrome = GameInfoTypes.BUILDING_BYZANTIUM_HIPPODROME
+local eCivilizationByzantium = GameInfoTypes.CIVILIZATION_BYZANTIUM
+local eBuildingDummyHorseYields = GameInfoTypes.BUILDING_DUMMYHORSEYIELDS
+local fGameSpeedModifier = GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].GoldenAgePercent / 100
+				
+-- adds WLTKD and Yields to all Horses in empire after its construction
+function OnConstructionAddBonuses(iPlayer, iCity, iBuilding)
 	local pPlayer = Players[iPlayer]
 
-	if pPlayer:GetName() == "Theodora" then
-		if iBuilding == GameInfoTypes.BUILDING_BYZANTIUM_HIPPODROME then
-			for pCity in pPlayer:Cities() do
-				local iGameSpeedModifier = GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].GoldenAgePercent / 100
-				local iWLTKDLength = math.floor(20 * iGameSpeedModifier)
+	if pPlayer:GetCivilizationType() == eCivilizationByzantium then
+		if iBuilding == eBuildingHippodrome then
+			for city in pPlayer:Cities() do
+				local iWLTKDLength = math.floor(20 * fGameSpeedModifier)
 				local pCapital = pPlayer:GetCapitalCity()
 				
-				pCity:SetNumRealBuilding(GameInfoTypes.BUILDING_HIPPO_DUMMY, 1)
-				pCity:ChangeWeLoveTheKingDayCounter(iWLTKDLength)
+				city:ChangeWeLoveTheKingDayCounter(iWLTKDLength)
 				
+				city:SetNumRealBuilding(eBuildingDummyHorseYields, 1)
+
 				if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
-					pPlayer:AddNotification(0, 'The City of [COLOR_POSITIVE_TEXT]'..pCapital:GetName()..'[ENDCOLOR] constructed Hippodrome. People love you. '..iWLTKDLength..'-turn WLTKD has started in '..pCity:GetName()..'.', 'Hippodrome constructed in '..pCapital:GetName(), pCity:GetX(), pCity:GetY())
+					pPlayer:AddNotification(0, 'The City of [COLOR_POSITIVE_TEXT]'..pCapital:GetName()..'[ENDCOLOR] constructed Hippodrome. People love you. '..iWLTKDLength..'-turn WLTKD has started in '..city:GetName()..'.', 'Hippodrome constructed in '..pCapital:GetName(), city:GetX(), city:GetY())
 				end
 			end
 		end
 	end
 end
 
-function HippodromeHooliganism(eTeam, eEra, bFirst)
+-- sets Anarchy and WLTKD on new Era
+function OnEraSetAnarchyAndWLTKD(eTeam, eEra, bFirst)
 	for id, pPlayer in pairs(Players) do
-		if pPlayer:IsEverAlive() then
-			if pPlayer:GetTeam() == eTeam then
-				if pPlayer:GetName() == "Theodora" then
-					local pCapital = pPlayer:GetCapitalCity()
+		if pPlayer:IsEverAlive() and pPlayer:GetTeam() == eTeam and pPlayer:GetCivilizationType() == eCivilizationByzantium then
+			local pCapital = pPlayer:GetCapitalCity()
 					
-					if pCapital:IsHasBuilding(GameInfoTypes.BUILDING_BYZANTIUM_HIPPODROME) then
-						if not pPlayer:IsAnarchy() then
-							local iGameSpeedModifier = GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].GoldenAgePercent / 100
-							local iWLTKDLength = math.floor(10 * iGameSpeedModifier) + 1
+			if pCapital:IsHasBuilding(eBuildingHippodrome) then
+				if not pPlayer:IsAnarchy() then
+					local fGameSpeedModifier = GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].GoldenAgePercent / 100
+					local iWLTKDLength = math.floor(10 * fGameSpeedModifier) + 1
 						
-							pPlayer:ChangeAnarchyNumTurns(2)
-							pCapital:ChangeWeLoveTheKingDayCounter(iWLTKDLength)
+					pPlayer:ChangeAnarchyNumTurns(2)
+					pCapital:ChangeWeLoveTheKingDayCounter(iWLTKDLength)
 						
-							if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
-								pPlayer:AddNotification(0, 'Your Empire entered new Era, causing a massive riot across your Empire. Your Empire falls into 1 turn of [ICON_RESISTANCE] Resistance, after which your [ICON_CITIZEN] Citizens will [ICON_HAPPINESS_1] love their king for '..(iWLTKDLength - 1)..' turns.', 'Hooliganism in '..pCapital:GetName()..'!', pCapital:GetX(), pCapital:GetY())
-							end
-						
-							break
-						end
+					if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+						pPlayer:AddNotification(0, 'Your Empire entered new Era, causing a massive riot across your Empire. Your Empire falls into 1 turn of [ICON_RESISTANCE] Resistance, after which your [ICON_CITIZEN] Citizens will [ICON_HAPPINESS_1] love their king for '..(iWLTKDLength - 1)..' turns.', 'Hooliganism in '..pCapital:GetName()..'!', pCapital:GetX(), pCapital:GetY())
 					end
+						
+					break
 				end
 			end
 		end
 	end
 end
 
+-- removes bonus gold and culture to horses on empire if Hippodrome is destroyed
+function OnCaptureRemoveHorseYields(iOldOwner, bIsCapital, iX, iY, iNewOwner, iPop, bConquest)
+	local pOldPlayer = Players[iOldOwner]
+	
+	if pOldPlayer:GetCivilizationType() ~= eCivilizationByzantium or not bIsCapital then return end
+
+	for city in pOldPlayer:Cities() do
+		city:SetNumRealBuilding(eBuildingDummyHorseYields, 0)
+	end
+end
+	
 function PositionCalculator(i1, i2)
 	return HexToWorld(ToHexFromGrid(Vector2(i1, i2)))
 end
 
---remove bonus gold and culture to horses on empire if hippodrome is destroyed
-function FallOfConstantinople(iOldOwner, bIsCapital, iX, iY, iNewOwner, iPop, bConquest)
-	local pPlayer = Players[iOldOwner]
-	local cCity = plot:GetPlotCity()
-	if pPlayer:GetName() == "Theodora" then --If not a Byzantine city then go no further
-		if cCity:IsCapital() then --only need to check if Byzantine capital was captured. Hippodrome can only be built in capital
-			for pCity in pPlayer:Cities() do
-				
-				pCity:SetNumRealBuilding(GameInfoTypes.BUILDING_HIPPO_DUMMY, 0)
-			end
-		end
-	end
-end		
-GameEvents.CityConstructed.Add(HippodromeBuilt)
-GameEvents.TeamSetEra.Add(HippodromeHooliganism)
-GameEvents.CityCaptureComplete.Add(FallOfConstantinople)
+GameEvents.CityConstructed.Add(OnConstructionAddBonuses)
+GameEvents.TeamSetEra.Add(OnEraSetAnarchyAndWLTKD)
+GameEvents.CityCaptureComplete.Add(OnCaptureRemoveHorseYields)
