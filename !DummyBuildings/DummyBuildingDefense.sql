075b339b17b2b914674c864ea63f0de57fde529b
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-03-18 15:49:19 -0600

    Update DummyBuildingDefense.sql
    added 1 defense dummy building

--- !DummyBuildings/DummyBuildingDefense.sql
+++ !DummyBuildings/DummyBuildingDefense.sql
@@ -6,7 +6,8 @@
 ------------------------------
 INSERT INTO BuildingClasses 	
 			(Type,							DefaultBuilding,				NoLimit)
-VALUES		('BUILDINGCLASS_DUMMYDEFENSE',	'BUILDING_DUMMYDEFENSE',		1);
+VALUES		('BUILDINGCLASS_DUMMYDEFENSE',	'BUILDING_DUMMYDEFENSE',		1),
+		('BUILDINGCLASS_DUMMY_1_DEFENSE',	'BUILDING_DUMMY_1_DEFENSE',		1);
 --==========================================================================================================================	
 
 --==========================================================================================================================	
@@ -17,6 +18,7 @@ VALUES		('BUILDINGCLASS_DUMMYDEFENSE',	'BUILDING_DUMMYDEFENSE',		1);
 ------------------------------	
 INSERT INTO Buildings 	
 			(Type,						BuildingClass,					Description,					GoldMaintenance,	Cost,	FaithCost,	GreatWorkCount, NeverCapture,	NukeImmune, ConquestProb,	HurryCostModifier,	IconAtlas,			PortraitIndex,	Defense)
-VALUES		('BUILDING_DUMMYDEFENSE',	'BUILDINGCLASS_DUMMYDEFENSE',	'TXT_KEY_BUILDING_DUMMYDEFENSE', 0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0,				500);
+VALUES		('BUILDING_DUMMYDEFENSE',	'BUILDINGCLASS_DUMMYDEFENSE',	'TXT_KEY_BUILDING_DUMMYDEFENSE', 0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0,				500),
+		('BUILDING_DUMMY_1_DEFENSE',	'BUILDINGCLASS_DUMMY_1_DEFENSE',	'TXT_KEY_BUILDING_DUMMY_1_DEFENSE', 0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0,				100);			
 --==========================================================================================================================
 --==========================================================================================================================
