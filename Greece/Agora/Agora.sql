2a9f5eff8aff7bc5d692a1f3e15da0b0ff705bdf
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-03-24 16:58:39 -0600

    Update Agora.sql

--- Greece/Agora/Agora.sql
+++ Greece/Agora/Agora.sql
@@ -30,7 +30,7 @@ VALUES	('BUILDING_GREECE_ATLAS', 			256, 		'AgoraPicture_256.dds',			1, 				1),
 --------------------------------	
 INSERT INTO UnitPromotions
 		(Type,				Description,				Help,						CannotBeChosen,	Sound,			PortraitIndex,		IconAtlas,		PediaType,	PediaEntry,				LostWithUpgrade, TradeMissionInfluenceModifier)
-VALUES		('PROMOTION_GREECE_PROXENOS',	'TXT_KEY_PROMOTION_GREECE_PROXENOS',	'TXT_KEY_PROMOTION_GREECE_PROXENOS_HELP',	1,		'AS2D_IF_LEVELUP',	9,			'PROMOTION_ATLAS',	'PEDIA_MELEE',	'TXT_KEY_PROMOTION_GREECE_PROXENOS',	0, 				5);
+VALUES		('PROMOTION_GREECE_PROXENOS',	'TXT_KEY_PROMOTION_GREECE_PROXENOS',	'TXT_KEY_PROMOTION_GREECE_PROXENOS_HELP',	1,		'AS2D_IF_LEVELUP',	42,			'PROMOTION_ATLAS',	'PEDIA_DIPLO',	'TXT_KEY_PROMOTION_GREECE_PROXENOS',	0, 				5);
 ------------------------------	
 -- UnitPromotions_UnitCombats
 ------------------------------		
