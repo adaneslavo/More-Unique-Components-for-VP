796c7550fd26a52e74dfcd7000a0cdfa4e28952a
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-04-15 14:08:57 -0600

    Update Khopesh.lua
    changed popup text slightly

--- Egypt/Khopesh/Khopesh.lua
+++ Egypt/Khopesh/Khopesh.lua
@@ -31,7 +31,7 @@ function RemoveFatigue(iPlayer)
 				if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
 					local vUnitPosition = PositionCalculator(pUnit:GetX(), pUnit:GetY())
 				
-					Events.AddPopupTextEvent(vUnitPosition, "[COLOR_LIGHT_GREY]-20% [COMBAT_STRENGTH] Fatigue[ENDCOLOR]", 1)
+					Events.AddPopupTextEvent(vUnitPosition, "[COLOR_LIGHT_GREY]-30% [COMBAT_STRENGTH] Fatigue[ENDCOLOR]", 1)
 				end
 			end
 		end
