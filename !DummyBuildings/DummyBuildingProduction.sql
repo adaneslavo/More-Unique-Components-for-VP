f5cc08d556a57f35dd218dd1d97dc8810c150b96
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-06-30 13:53:30 -0600

    Update DummyBuildingProduction.sql
    Added new Feitoria dummy, which modifies the base yields on the University of Coimbra wonder.
    Yields from the Feitoria's effect on Uni of Coimbra will now be exposed to the UI

--- !DummyBuildings/DummyBuildingProduction.sql
+++ !DummyBuildings/DummyBuildingProduction.sql
@@ -6,7 +6,8 @@
 ------------------------------
 INSERT INTO BuildingClasses 	
 			(Type,								DefaultBuilding,				NoLimit)
-VALUES		('BUILDINGCLASS_DUMMYPRODUCTION',	'BUILDING_DUMMYPRODUCTION',		1);
+VALUES		('BUILDINGCLASS_DUMMYPRODUCTION',	'BUILDING_DUMMYPRODUCTION',		1),
+		('BUILDINGCLASS_FEITORIADUMMY',		'BUILDING_FEITORIADUMMY',		1);
 --==========================================================================================================================	
 
 --==========================================================================================================================	
@@ -17,12 +18,20 @@ VALUES		('BUILDINGCLASS_DUMMYPRODUCTION',	'BUILDING_DUMMYPRODUCTION',		1);
 ------------------------------	
 INSERT INTO Buildings 	
 			(Type,							BuildingClass,						Description,						GoldMaintenance,	Cost,	FaithCost,	GreatWorkCount, NeverCapture,	NukeImmune, ConquestProb,	HurryCostModifier,	IconAtlas,			PortraitIndex)
-VALUES		('BUILDING_DUMMYPRODUCTION',	'BUILDINGCLASS_DUMMYPRODUCTION',	'TXT_KEY_BUILDING_DUMMYPRODUCTION',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0);
+VALUES		('BUILDING_DUMMYPRODUCTION',	'BUILDINGCLASS_DUMMYPRODUCTION',	'TXT_KEY_BUILDING_DUMMYPRODUCTION',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0),
+		('BUILDING_FEITORIADUMMY',	'BUILDINGCLASS_FEITORIADUMMY',	'TXT_KEY_BUILDING_DUMMYPRODUCTION',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0);
 ------------------------------	
 -- Building_YieldChanges
 ------------------------------		
 INSERT INTO Building_YieldChanges 	
 			(BuildingType, 					YieldType,			Yield)
 VALUES		('BUILDING_DUMMYPRODUCTION',	'YIELD_PRODUCTION',	1);
+------------------------------	
+-- Building_BuildingClassYieldChanges
+------------------------------
+INSERT INTO Building_BuildingClassYieldChanges
+			(BuildingType,						BuildingClassType,				YieldType,		YieldChange)
+VALUES		('BUILDING_FEITORIADUMMY',	'BUILDINGCLASS_OXFORD_UNIVERSITY',	'YIELD_PRODUCTION',	1),
+		('BUILDING_FEITORIADUMMY',	'BUILDINGCLASS_OXFORD_UNIVERSITY',	'YIELD_GOLD',	2);
 --==========================================================================================================================
 --==========================================================================================================================
