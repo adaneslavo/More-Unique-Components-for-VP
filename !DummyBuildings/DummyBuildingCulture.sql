f51705e3d3a1f747d90208b95d829da9d6a6e753
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-07-03 10:28:32 -0600

    Update DummyBuildingCulture.sql
    added dummy building which adds 1 culture onto local castle (this will allow the Qila's culture from city defense to be exposed to UI)

--- !DummyBuildings/DummyBuildingCulture.sql
+++ !DummyBuildings/DummyBuildingCulture.sql
@@ -6,7 +6,8 @@
 ------------------------------
 INSERT INTO BuildingClasses 	
 			(Type,							DefaultBuilding,				NoLimit)
-VALUES		('BUILDINGCLASS_DUMMYCULTURE',	'BUILDINGCLASS_DUMMYCULTURE',	1);
+VALUES		('BUILDINGCLASS_DUMMYCULTURE',	'BUILDINGCLASS_DUMMYCULTURE',	1),
+		('BUILDINGCLASS_DUMMYCASTLECULTURE',	'BUILDING_DUMMYCASTLECULTURE',	1);
 --==========================================================================================================================	
 
 --==========================================================================================================================	
@@ -17,7 +18,9 @@ VALUES		('BUILDINGCLASS_DUMMYCULTURE',	'BUILDINGCLASS_DUMMYCULTURE',	1);
 ------------------------------	
 INSERT INTO Buildings 	
 			(Type,						BuildingClass,					Description,						GoldMaintenance,	Cost,	FaithCost,	GreatWorkCount, NeverCapture,	NukeImmune, ConquestProb,	HurryCostModifier,	IconAtlas,			PortraitIndex)
-VALUES		('BUILDING_DUMMYCULTURE',	'BUILDINGCLASS_DUMMYCULTURE',	'TXT_KEY_BUILDING_DUMMYCULTURE',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0);
+VALUES		('BUILDING_DUMMYCULTURE',	'BUILDINGCLASS_DUMMYCULTURE',	'TXT_KEY_BUILDING_DUMMYCULTURE',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0),
+		('BUILDING_DUMMYCASTLECULTURE',	'BUILDINGCLASS_DUMMYCASTLECULTURE',	'TXT_KEY_BUILDING_DUMMYCULTURE',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0);
+
 ------------------------------	
 -- Building_YieldChanges
 ------------------------------		
@@ -26,3 +29,9 @@ INSERT INTO Building_YieldChanges
 VALUES		('BUILDING_DUMMYCULTURE',	'YIELD_CULTURE',	1);
 --==========================================================================================================================
 --==========================================================================================================================
+------------------------------	
+-- Building_BuildingClassLocalYieldChanges
+------------------------------
+INSERT INTO Building_BuildingClassLocalYieldChanges
+			(BuildingType,					BuildingClassType,		YieldType,		YieldChange)
+VALUES		('BUILDING_DUMMYCASTLECULTURE',	'BUILDINGCLASS_CASTLE', 'YIELD_CULTURE',	1);
