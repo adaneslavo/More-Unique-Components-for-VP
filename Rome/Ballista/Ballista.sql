872b2e6d148a3a7750866077f3686598b835d96a
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-07-17 15:07:59 -0600

    Update Ballista.sql
    simplified ballista legatus bonus. now just 1 movement (they were just way too fast before)

--- Rome/Ballista/Ballista.sql
+++ Rome/Ballista/Ballista.sql
@@ -39,24 +39,9 @@ UPDATE Unit_Flavors SET Flavor = '6' WHERE UnitType = 'UNIT_ROMAN_BALLISTA' AND 
 -- UnitPromotions
 ------------------------------
 INSERT INTO UnitPromotions 
-			(Type, 													Description, 												Help, 																Sound, 				River,	CombatPercent,	MovesChange,	AttackMod,	DefenseMod, ExperiencePercent,	IgnoreZOC,			LostWithUpgrade,	CannotBeChosen, PortraitIndex, 	IconAtlas, 			PediaType, 		PediaEntry)
-VALUES		('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA_HELP',	'AS2D_IF_LEVELUP',	0,		0,				0,				0,			0,			0,					0,					0, 					1,				33, 			'PROMOTION_ATLAS', 	'PEDIA_RANGED',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA'),
-			('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',			'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',		'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT_HELP',			'AS2D_IF_LEVELUP',	1,		0,				0,				0,			0,			0,					0,					0, 					1,				33, 			'PROMOTION_ATLAS', 	'PEDIA_RANGED',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT');
-------------------------------
--- UnitPromotions_Terrains
-------------------------------
-INSERT INTO UnitPromotions_Terrains
-			(PromotionType, 								TerrainType,		DoubleMove)
-VALUES		('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',	'TERRAIN_GRASS',	1),
-			('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',	'TERRAIN_PLAINS',	1),
-			('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',	'TERRAIN_TUNDRA',	1);
-------------------------------
--- UnitPromotions_Features
-------------------------------
-INSERT INTO UnitPromotions_Features
-			(PromotionType, 								FeatureType,			DoubleMove)
-VALUES		('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',	'FEATURE_FLOOD_PLAINS',	1),
-			('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',	'FEATURE_OASIS',		1);
+		(Type, 							Description, 							Help, 									Sound, 			CombatPercent,	MovesChange,	AttackMod,	DefenseMod, ExperiencePercent,	IgnoreZOC,	LostWithUpgrade,	CannotBeChosen, PortraitIndex, 	IconAtlas, 		PediaType, 		PediaEntry)
+VALUES		('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA_HELP',	'AS2D_IF_LEVELUP',	0,		0,		0,		0,		0,		0,		0, 			1,		33, 		'PROMOTION_ATLAS', 	'PEDIA_RANGED',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_ON_BALLISTA'),
+		('PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',		'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT',		'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT_HELP',		'AS2D_IF_LEVELUP',	0,		1,		0,		0,		0,		0,		0, 			1,		33, 		'PROMOTION_ATLAS', 	'PEDIA_RANGED',	'TXT_KEY_PROMOTION_UNIT_ROME_LEGATUS_LEGIONIS_EFFECT');
 --------------------------------	
 -- Unit_FreePromotions
 --------------------------------
