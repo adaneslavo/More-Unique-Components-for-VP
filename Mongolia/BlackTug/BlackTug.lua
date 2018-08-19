4888f752bfc24aff86f44bdb5d8afc39a4f92b38
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-08-19 14:05:28 -0600

    Update BlackTug.lua
    disabled lua for adding massacre promotion onto Black Tug

--- Mongolia/BlackTug/BlackTug.lua
+++ Mongolia/BlackTug/BlackTug.lua
@@ -2,30 +2,30 @@
 -- Author: adan_eslavo
 -- DateCreated: 29/10/2017
 --------------------------------------------------------------
-function PillagedTileBonusMove(iPlayer, iUnit, iX, iY)
-	local pPlayer = Players[iPlayer]
-	local pUnit = pPlayer:GetUnitByID(iUnit)
+--function PillagedTileBonusMove(iPlayer, iUnit, iX, iY)
+--	local pPlayer = Players[iPlayer]
+--	local pUnit = pPlayer:GetUnitByID(iUnit)
+--
+--	if pUnit and pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MINGGHAN) then
+--		local pPlot = Map.GetPlot(iX, iY)
+--		
+--		pUnit:SetHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MASSACRE, pPlot:IsImprovementPillaged())
+--	end
+--end
 
-	if pUnit and pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MINGGHAN) then
-		local pPlot = Map.GetPlot(iX, iY)
-		
-		pUnit:SetHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MASSACRE, pPlot:IsImprovementPillaged())
-	end
-end
-
-function OnPillageBonus(iX, iY, iOwner, iOldImprovement, iNewImprovement, bPillaged) 
-	if bPillaged then
-		local pPlot = Map.GetPlot(iX, iY)
-		
-		for iVal = 0,(pPlot:GetNumUnits() - 1) do
-			local pUnit = pPlot:GetUnit(iVal)
-		
-			if pUnit and pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MINGGHAN) then	
-				pUnit:SetHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MASSACRE, true)
-			end
-		end
-	end
-end
-
-GameEvents.UnitSetXY.Add(PillagedTileBonusMove)
-GameEvents.TileImprovementChanged.Add(OnPillageBonus)
+--function OnPillageBonus(iX, iY, iOwner, iOldImprovement, iNewImprovement, bPillaged) 
+--	if bPillaged then
+--		local pPlot = Map.GetPlot(iX, iY)
+--		
+--		for iVal = 0,(pPlot:GetNumUnits() - 1) do
+--			local pUnit = pPlot:GetUnit(iVal)
+--		
+--			if pUnit and pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MINGGHAN) then	
+--				pUnit:SetHasPromotion(GameInfoTypes.PROMOTION_UNIT_MONGOLIA_MASSACRE, true)
+--			end
+--		end
+--	end
+--end
+--
+--GameEvents.UnitSetXY.Add(PillagedTileBonusMove)
+--GameEvents.TileImprovementChanged.Add(OnPillageBonus)
