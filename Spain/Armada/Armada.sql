b39fa8216597ce52951b3d7f3bd6bcef48a3ac19
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-08-19 14:29:23 -0600

    Update Armada.sql
    added armada PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL, which gives the 2XP from spotting (optimizing code)

--- Spain/Armada/Armada.sql
+++ Spain/Armada/Armada.sql
@@ -73,9 +73,10 @@ VALUES		('UNIT_SPAIN_ATLAS', 		256, 		'ArmadaPicture_256.dds',	1, 				1),
 -- UnitPromotions
 ------------------------------
 INSERT INTO UnitPromotions 
-			(Type, 											Description, 										Help, 														Sound, 				CombatPercent,	MovesChange,	AttackMod,	DefenseMod, ExperiencePercent,	IgnoreZOC,	LostWithUpgrade,	CannotBeChosen, PortraitIndex, 	IconAtlas, 							PediaType, 		PediaEntry)
-VALUES		('PROMOTION_UNIT_SPAIN_SANTA_MARIA', 			'TXT_KEY_PROMOTION_UNIT_SPAIN_SANTA_MARIA',			'TXT_KEY_PROMOTION_UNIT_SPAIN_SANTA_MARIA_HELP', 			'AS2D_IF_LEVELUP',	0,				0,				0,			0,			0,					0,			0,					1, 				16, 			'PROMOTION_ATLAS', 					'PEDIA_NAVAL', 'TXT_KEY_PROMOTION_UNIT_SPAIN_SANTA_MARIA'),
-			('PROMOTION_UNIT_SPAIN_INVINCIBLE', 			'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE',			'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE_HELP', 			'AS2D_IF_LEVELUP',	0,				0,				0,			0,			0,					0,			1,					1, 				0, 				'UNIT_BABYLON_PROMOTION_5_ATLAS', 	'PEDIA_NAVAL', 'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE');
+			(Type, 						Description, 							Help, 									Sound, 			GainsXPFromSpotting, LostWithUpgrade,	CannotBeChosen, PortraitIndex, 	IconAtlas, 				PediaType, 	PediaEntry)
+VALUES		('PROMOTION_UNIT_SPAIN_SANTA_MARIA', 			'TXT_KEY_PROMOTION_UNIT_SPAIN_SANTA_MARIA',			'TXT_KEY_PROMOTION_UNIT_SPAIN_SANTA_MARIA_HELP', 			'AS2D_IF_LEVELUP',	0, 			0,			1, 		16, 		'PROMOTION_ATLAS', 			'PEDIA_NAVAL', 'TXT_KEY_PROMOTION_UNIT_SPAIN_SANTA_MARIA'),
+		('PROMOTION_UNIT_SPAIN_INVINCIBLE', 			'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE',			'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE_HELP', 			'AS2D_IF_LEVELUP',	0, 			1,			1, 		0, 		'UNIT_BABYLON_PROMOTION_5_ATLAS', 	'PEDIA_NAVAL', 'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE'),
+		('PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL', 		'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL',		'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL_HELP', 		'AS2D_IF_LEVELUP',	1, 			1,			1, 		0, 		'UNIT_BABYLON_PROMOTION_5_ATLAS', 	'PEDIA_NAVAL', 'TXT_KEY_PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL');
 ------------------------------
 -- UnitPromotions_YieldFromKills
 ------------------------------
