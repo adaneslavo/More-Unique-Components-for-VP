4a284c1003570e3afce28b5efbd0ada50e790d88
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-08-19 14:15:30 -0600

    Update Armada.lua
    changed mechanics for invincible armada. 
    re-added 'PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL', which gives XPfromSpotting via sql, instead of checking plot ownership with lua.

--- Spain/Armada/Armada.lua
+++ Spain/Armada/Armada.lua
@@ -12,21 +12,21 @@ function InvincibleArmada(iPlayer)
 				local iY = pUnit:GetY()
 				local pPlot = Map.GetPlot(iX, iY)
 
-				--pUnit:SetHasPromotion(GameInfoTypes["PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL"], true)
+				pUnit:SetHasPromotion(GameInfoTypes["PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL"], true)
 				pUnit:SetBaseCombatStrength(44)
 				
-				if pPlot:GetOwner() ~= -1 then
-					local pEnemyPlayer = Players[pPlot:GetOwner()]
-					local iTeam = pPlayer:GetTeam()
-					local pEnemyTeam = Teams[pEnemyPlayer:GetTeam()]
-
-					if pEnemyTeam:IsAtWar(iTeam) then
-						pUnit:ChangeExperience(2)
-					end
-				end
+--				if pPlot:GetOwner() ~= -1 then
+--					local pEnemyPlayer = Players[pPlot:GetOwner()]
+--					local iTeam = pPlayer:GetTeam()
+--					local pEnemyTeam = Teams[pEnemyPlayer:GetTeam()]
+--
+--					if pEnemyTeam:IsAtWar(iTeam) then
+--						pUnit:ChangeExperience(2)
+--					end
+--				end
 			else
 				pUnit:SetBaseCombatStrength(40)
-				--pUnit:SetHasPromotion(GameInfoTypes["PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL"], false)
+				pUnit:SetHasPromotion(GameInfoTypes["PROMOTION_UNIT_SPAIN_INVINCIBLE_AT_FULL"], false)
 			end
 		end
 	end
