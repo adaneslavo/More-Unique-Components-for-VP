46e240a7d180d808f0ea16bb7c5d1c332f4b31a2
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-08-26 14:58:16 -0600

    Update DummyBuildingGPPGoldAndCulture.sql
    changed Gold and Culture to a BuildingClass modifier, so the yields are exposed to the UI for the Riad

--- !DummyBuildings/DummyBuildingGPPGoldAndCulture.sql
+++ !DummyBuildings/DummyBuildingGPPGoldAndCulture.sql
@@ -19,11 +19,11 @@ INSERT INTO Buildings
 			(Type,								BuildingClass,							Description,								GoldMaintenance,	Cost,	FaithCost,	GreatWorkCount, NeverCapture,	NukeImmune, ConquestProb,	HurryCostModifier,	IconAtlas,			PortraitIndex,	GreatPeopleRateChange,	SpecialistType)
 VALUES		('BUILDING_DUMMYGPPGOLDANDCULTURE',	'BUILDINGCLASS_DUMMYGPPGOLDANDCULTURE',	'TXT_KEY_BUILDING_DUMMYGPPGOLDANDCULTURE',	0,					-1,		-1,			-1,				1,				1,			0,				-1,					'CIV_COLOR_ATLAS',	0,				2,						'SPECIALIST_MERCHANT');
 ------------------------------	
--- Building_YieldChanges
-------------------------------		
-INSERT INTO Building_YieldChanges 	
-			(BuildingType, 			YieldType,			Yield)
-VALUES		('BUILDING_DUMMYGOLD',	'YIELD_GOLD',		2),
-			('BUILDING_DUMMYGOLD',	'YIELD_CULTURE',	2);
+-- Building_BuildingClassLocalYieldChanges
+------------------------------
+INSERT INTO Building_BuildingClassLocalYieldChanges
+			(BuildingType,					BuildingClassType,		YieldType,		YieldChange)
+VALUES		('BUILDING_DUMMYGPPGOLDANDCULTURE',	'BUILDINGCLASS_GARDEN', 'YIELD_CULTURE',	2),
+		('BUILDING_DUMMYGPPGOLDANDCULTURE',	'BUILDINGCLASS_GARDEN', 'YIELD_GOLD',		2);
 --==========================================================================================================================
 --==========================================================================================================================
