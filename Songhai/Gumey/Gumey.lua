ac4620e6c35486437dd2ee547da4483f38ad451b
adan_eslavo <adan_eslavo@o2.pl>
2018-08-29 20:54:20 +0200

    v36.6.1
    Many fixes. Gumey bringed back to state of usefullness.

--- Songhai/Gumey/Gumey.lua
+++ Songhai/Gumey/Gumey.lua
@@ -3,7 +3,7 @@
 -- DateCreated: 25/01/2018
 -- 2018-02-04 Modified by Infixo
 --------------------------------------------------------------
-local MonopolyResources = {
+local tMonopolyResources = {
 	GameInfoTypes.RESOURCE_ALUMINUM,
 	GameInfoTypes.RESOURCE_COAL,
 	GameInfoTypes.RESOURCE_HORSE,
@@ -47,36 +47,24 @@ local MonopolyResources = {
 	GameInfoTypes.RESOURCE_WINE,
 	}
 
-<<<<<<< HEAD
-function OnPlayerDoTurnQadi (iPlayer)
+local eBuildingGumey = GameInfoTypes.BUILDING_SONGHAI_GUMEY
+local eBuildingDummyForGumey = GameInfoTypes.BUILDING_D_FOR_GUMEY
+
+function OnTurnAddMonopolyBonus (iPlayer)
 	local pPlayer = Players[iPlayer]
-	local MonopolyCounter = 0
-	local GumeyBuilding = GameInfoTypes.BUILDING_SONGHAI_GUMEY
-	local MonopolyDummy = GameInfoTypes.BUILDING_DUMMYSONGHAICLASSYIELD
+	local iMonopolyCounter = 0
 	
-	for _, iResource in pairs(MonopolyResources) do
-		if (pPlayer:GetNumResourceTotal(iResource) / Map:GetNumResources(iResource)) >= 0.51 then
-			MonopolyCount = MonopolyCount +1
+	for i, resource in pairs(tMonopolyResources) do
+		if (pPlayer:GetNumResourceTotal(resource) / Map:GetNumResources(resource)) > 0.5 then
+			iMonopolyCounter = iMonopolyCounter +1
 		end
 	end
-	for cCity in nPlayer:Cities() do
-		if cCity:IsHasBuilding(GumeyBuilding) then
-			cCity:SetNumRealBuilding(MonopolyDummy, MonopolyCount)
-=======
-	if pPlayer and pPlayer:GetCivilizationType() == GameInfoTypes.CIVILIZATION_SONGHAI then
-		for pCity in pPlayer:Cities() do
-			if pCity:IsHasBuilding(GameInfoTypes.BUILDING_SONGHAI_QADI_COURT) then
-				-- culture
-				local iCultureChange = math.floor( 0.1 * pCity:GetBaseYieldRate(YieldTypes.YIELD_PRODUCTION) )
-				pCity:SetNumRealBuilding(GameInfoTypes.BUILDING_DUMMYCULTURE, iCultureChange)
-				-- gold for non-followers
-				if pPlayer:GetMajorityReligion() ~= -1 then
-					pCity:SetNumRealBuilding(GameInfoTypes.BUILDING_DUMMYCOURTHOUSEGOLD, pCity:GetPopulation() - pCity:GetNumFollowers(pPlayer:GetMajorityReligion()))
-				end
-			end
->>>>>>> 4f05c6b7a1d5177869f3f2e86971519de6bc805d
+	
+	for city in nPlayer:Cities() do
+		if city:IsHasBuilding(eBuildingGumey) then
+			city:SetNumRealBuilding(eBuildingDummyForGumey, iMonopolyCounter)
 		end
 	end
 end
 
-GameEvents.PlayerDoTurn.Add(OnPlayerDoTurnQadi)
+GameEvents.PlayerDoTurn.Add(OnTurnAddMonopolyBonus)
