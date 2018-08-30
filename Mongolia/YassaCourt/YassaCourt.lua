5e16ce0f478612e20a333bdeb687135af6ea46be
Adam <33016300+adaneslavo@users.noreply.github.com>
2018-08-30 06:45:57 +0200

    Update YassaCourt.lua

--- Mongolia/YassaCourt/YassaCourt.lua
+++ Mongolia/YassaCourt/YassaCourt.lua
@@ -55,7 +55,7 @@ function OnTechResearchedBuildYassa(iTeam, iTech)
 	if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
 		pPlayer:AddNotification(0, 
 			'Player researched Philosophy and received free Yassa Court in every [COLOR_CYAN]non-occupied city[ENDCOLOR].', 
-			'Yassa Court built in '..pCity:GetName(), 
+			'Yassa Courts built across the Empire, 
 			pCity:GetX(), pCity:GetY())
 	end
 end
