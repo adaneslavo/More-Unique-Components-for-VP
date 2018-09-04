201d5e0e69161a46ae4ffd5c6ba781bc21e04d06
adan_eslavo <adan_eslavo@o2.pl>
2018-09-04 20:29:24 +0200

    v36.8 part X
    White Tower, Teutonic Order and Agora. White Tower fix.

--- Greece/Agora/Agora.lua
+++ Greece/Agora/Agora.lua
@@ -4,28 +4,30 @@
 --------------------------------------------------------------
 local eBuildingDummyForProxenos = GameInfoTypes.BUILDING_D_FOR_PROXENOS
 local ePromotionProxenos = GameInfoTypes.PROMOTION_UNIT_GREECE_PROXENOS
+local eCivilizationGreece = GameInfoTypes.CIVILIZATION_GREECE
+
 -- adds gold to the city after performing diplo missions
 function OnUnitPrekillProxenosGold(unitOwnerId, unitId, iUnitType, unitX, unitY, bDelay, eKillingPlayer)
-	--print("ProxenosGold",unitOwnerId, unitId, iUnitType, unitX, unitY, bDelay, eKillingPlayer)
 	if not (bDelay and eKillingPlayer == -1) then return end -- this check is important as this event is called TWICE; eKillingPlayer is always -1 when bDelay = false, only when bDelay = true it contains a correct player ID
 
 	local pPlayer = Players[unitOwnerId]
-	if not pPlayer then return end -- assert
+	
+	if not (pPlayer and pPlayer == eCivilizationGreece) then return end
 
 	local pUnit = pPlayer:GetUnitByID(unitId)
-	if not pUnit then return end -- assert
-
-	if pUnit:IsHasPromotion(ePromotionProxenos) then
+	
+	if pUnit and pUnit:IsHasPromotion(ePromotionProxenos) then
 		local pPlot = Map.GetPlot(unitX, unitY)
 		local eCiv = pPlot:GetOwner()
 
 		if eCiv ~= -1 and Players[eCiv]:IsMinorCiv() then
 			local pCapital = pPlayer:GetCapitalCity()
-			local sName = pCapital:GetName()
-
+			
 			pCapital:SetNumRealBuilding(eBuildingDummyForProxenos, pCapital:GetNumRealBuilding(eBuildingDummyForProxenos) + 1)
 			
 			if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
+				local sName = pCapital:GetName()
+
 				pPlayer:AddNotification(NotificationTypes.NOTIFICATION_INSTANT_YIELD,
 					'[ICON_INFLUENCE] Diplomatic Mission performed by Unit with Proxenos ability. The City of '..sName..' gained additional 2 [ICON_GOLD] Gold',
 					'Gold added in '..sName, 
