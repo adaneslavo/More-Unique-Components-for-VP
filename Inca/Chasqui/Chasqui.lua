ac233693b35fee0b5f860c004d23f4cba739b7c0
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-11-10 14:12:10 -0700

    Update Chasqui.lua
    changed from +15 HP heal on turn (not stacking with fortify) to +1 move if in friendly territory

--- Inca/Chasqui/Chasqui.lua
+++ Inca/Chasqui/Chasqui.lua
@@ -9,27 +9,15 @@ local ePromotionChasquiwasi = GameInfoTypes.PROMOTION_UNIT_INCA_CHASQUIWASI
 
 -- heals unit when in firendly territory and not fortfied
 function Chasquiwasi(iPlayer)
-    local pPlayer = Players[iPlayer]
+	local pPlayer = Players[iPlayer]
 
-    for pUnit in pPlayer:Units() do
-        if pUnit:IsHasPromotion(ePromotionChasquiwasi) then
-            local pPlot = Map.GetPlot(pUnit:GetX(), pUnit:GetY())
-
-            if pPlot:GetOwner() == iPlayer and pUnit:FortifyModifier() <= 0 then
-				pUnit:ChangeDamage(-15)
-
-				if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
-					local vUnitPosition = PositionCalculator(pUnit:GetX(), pUnit:GetY())
-
-					Events.AddPopupTextEvent(vUnitPosition, "[COLOR_POSITIVE_TEXT]Chasquiwasi[ENDCOLOR]", 2)
-				end
-            end
-        end
-    end
-end
-
-function PositionCalculator(i1, i2)
-	return HexToWorld(ToHexFromGrid(Vector2(i1, i2)))
+	for pUnit in pPlayer:Units() do
+		if pUnit:IsHasPromotion(GameInfoTypes.PROMOTION_UNIT_INCA_CHASQUIWASI) then
+			if Map.GetPlot(pUnit:GetX(), pUnit:GetY()):GetOwner() == iPlayer then
+				pUnit:ChangeMoves(60)
+			end
+		end
+	end
 end
 
 GameEvents.PlayerDoTurn.Add(Chasquiwasi)
