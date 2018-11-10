5089e0aa567021371f9b93ef2c2405f02355fe1d
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-11-10 13:59:20 -0700

    Update Tambo.lua
    implemented Coca monopoly and integration with monopoly.
    
    Coca now appears under the city, then adds and removes a plantation to connect it. The Coca is removed from the plot if the city is razed

--- Inca/TamboAndCoca/Tambo.lua
+++ Inca/TamboAndCoca/Tambo.lua
@@ -5,14 +5,45 @@
 -- DateCreated: 2018-02-03
 --------------------------------------------------------------
 
+local eResourceCoca = GameInfoTypes.RESOURCE_COCA
+local eCivilizationInca = GameInfoTypes.CIVILIZATION_INCA
+
+-- creates 2 sources of flax if possible. Checks 6 different conditions in total prioritizing player tiles over unowned ones
+function TamboPlaceCoca(iPlayer, iCity, iBuilding)
+	local pPlayer = Players[iPlayer]
+	
+	if not(pPlayer and pPlayer:GetCivilizationType() == eCivilizationInca) then return end
+	if iBuilding == GameInfoTypes.BUILDING_INCA_TAMBO then
+	
+		local pCity = pPlayer:GetCityByID(iCity)
+		local pPlot = Map.GetPlot(pCity:GetX(), pCity:GetY())
+		pPlot:SetResourceType(eResourceCoca, 1)
+		pPlot:SetImprovementType(GameInfoTypes.IMPROVEMENT_PLANTATION)
+		pPlot:SetImprovementType(-1)
+	end
+end
+
+function CocaRemoveOnRaze(hexPos, iOldOwner, cityID, iNewOwner)
+	local pPlayer = Players[iOldOwner]
+	local pCity = pPlayer:GetCityByID(cityID)
+	local pPlot = Map.GetPlot(pCity:GetX(), pCity:GetY())
+	
+	if pPlot:GetResourceType() == eResourceCoca then
+		pPlot:SetResourceType(-1)
+	end
+end
+
+
 function CocaMonopoly(iPlayer)
 	local pPlayer = Players[iPlayer]
 	
 	for unit in pPlayer:Units() do
-		if unit:GetDomainType() = GameInfoTypes.DOMAIN_LAND then unit:SetHasPromotion(GameInfoTypes.PROMOTION_COCA, pPlayer.HasGlobalMonopoly(GameInfoTypes.RESOURCE_COCA)) end
+		if unit:GetDomainType() == GameInfoTypes.DOMAIN_LAND then unit:SetHasPromotion(GameInfoTypes.PROMOTION_COCA, pPlayer:HasGlobalMonopoly(eResourceCoca)) end
 	end
 end
 
-if Game.IsCivEverActive(GameInfoTypes.CIVILIZATION_INCA) then
+if Game.IsCivEverActive(eCivilizationInca) then
+	GameEvents.CityConstructed.Add(TamboPlaceCoca)
+	Events.SerialEventCityDestroyed.Add(CocaRemoveOnRaze)
 	GameEvents.PlayerDoTurn.Add(CocaMonopoly)
 end
