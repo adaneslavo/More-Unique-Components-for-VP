411c6b3a0805de938169c5bb49beca82380c3734
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-11-12 14:05:47 -0700

    Update Qullqa.sql

--- Inca/Tambo/Qullqa.sql
+++ Inca/Tambo/Qullqa.sql
@@ -16,7 +16,7 @@ VALUES	('CIVILIZATION_INCA', 		'BUILDINGCLASS_GRANARY', 	'BUILDING_INCA_QULLQA')
 INSERT INTO Buildings 	
 		(Type, 					BuildingClass, Cost, GoldMaintenance, PrereqTech, Water, TradeRouteLandDistanceModifier, AlwaysHeal, 	FoodKept, TradeRouteLandGoldBonus, AllowsFoodTradeRoutes, CityConnectionGoldModifier, 	FinishLandTRTourism, Description, 					Civilopedia, 						Help, 								Strategy,								PlotBuyCostModifier, ArtDefineTag, 	ArtInfoEraVariation, FreeStartEra, DisplayPosition, MinAreaSize, ConquestProb, 	GreatPeopleRateModifier, FreshWater, HurryCostModifier, PortraitIndex, 	IconAtlas)
 SELECT	'BUILDING_INCA_QULLQA',	BuildingClass, Cost, GoldMaintenance, PrereqTech, Water, TradeRouteLandDistanceModifier, 5,				FoodKept, TradeRouteLandGoldBonus, AllowsFoodTradeRoutes, CityConnectionGoldModifier,	FinishLandTRTourism, 'TXT_KEY_BUILDING_INCA_QULLQA', 'TXT_KEY_BUILDING_INCA_QULLQA_TEXT', 'TXT_KEY_BUILDING_INCA_QULLQA_HELP', 'TXT_KEY_BUILDING_INCA_QULLQA_STRATEGY',	PlotBuyCostModifier, ArtDefineTag,	ArtInfoEraVariation, FreeStartEra, DisplayPosition, MinAreaSize, ConquestProb, 	GreatPeopleRateModifier, FreshWater, HurryCostModifier, 0, 				'BUILDING_QULLQA_ATLAS'
-FROM Buildings WHERE Type = 'BUILDINGCLASS_GRANARY';	
+FROM Buildings WHERE Type = 'BUILDING_GRANARY';	
 ------------------------------	
 -- Building_Yields
 ------------------------------
@@ -29,7 +29,7 @@ VALUES		('BUILDING_INCA_QULLQA', 'YIELD_FOOD', 		1);
 INSERT INTO Building_ResourceYieldChanges
 			(BuildingType, 				ResourceType, YieldType, Yield)
 SELECT 		'BUILDING_INCA_QULLQA', 		ResourceType, YieldType, Yield
-FROM Building_ResourceYieldChanges WHERE BuildingType = 'BUILDINGCLASS_GRANARY';		
+FROM Building_ResourceYieldChanges WHERE BuildingType = 'BUILDING_GRANARY';		
 ------------------------------	
 -- Building_Flavors
 ------------------------------		
