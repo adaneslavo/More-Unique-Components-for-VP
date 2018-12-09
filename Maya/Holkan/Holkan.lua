46ca6306286fbab58d44cd079656aa5dc90b4318
pineappledan <33136027+pineappledan@users.noreply.github.com>
2018-12-09 07:37:27 -0700

    Update Holkan.lua
    added culture to Lost Codices, reduced from 10 science to 5 sci/5cul

--- Maya/Holkan/Holkan.lua
+++ Maya/Holkan/Holkan.lua
@@ -5,41 +5,23 @@
 include("FLuaVector.lua")
 
 local fGameSpeedModifier = GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].ResearchPercent / 100
---local ePromotionRenewalCycle = GameInfoTypes.PROMOTION_UNIT_MAYA_RENEWAL_CYCLE
 local ePromotionTreasureHunter2 = GameInfoTypes.PROMOTION_UNIT_MAYA_SCOUT_GOODY_BONUS_2
-
---[[function RenewalCycleOnHolkan(iPlayer)
-	local pPlayer = Players[iPlayer]
-	local iCounter = math.floor(20 * GameInfo.GameSpeeds[ Game.GetGameSpeedType() ].TrainPercent / 100)
-
-	if Game.GetElapsedGameTurns() % iCounter == 0 then
-		for pUnit in pPlayer:Units() do
-			if pUnit:IsHasPromotion(ePromotionRenewalCycle) then
-				pUnit:ChangeDamage(-30)
-				
-				if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
-					local vUnitPosition = PositionCalculator(pUnit:GetX(), pUnit:GetY())
-			
-					Events.AddPopupTextEvent(vUnitPosition, "[COLOR_POSITIVE_TEXT]Renewal Cycle[ENDCOLOR]", 1)
-				end
-			end
-		end
-	end
-end --]]
 
 function HolkansGoodyHuts(iPlayer, iUnit, eGoody, iX, iY)
 	local pPlayer = Players[iPlayer]
 	local pUnit = pPlayer:GetUnitByID(iUnit)
 
 	if pUnit:IsHasPromotion(ePromotionTreasureHunter2) then
-		local iResearchBonus = math.floor(10 * fGameSpeedModifier)
+		local iResearchBonus = math.floor(5 * fGameSpeedModifier)
 			
 		pPlayer:ChangeOverflowResearch(iResearchBonus)
+		pPlayer:ChangeJONSCulture(iResearchBonus)
 
 		if pPlayer:IsHuman() and pPlayer:IsTurnActive() then
 			local vUnitPosition = PositionCalculator(iX, iY)
 
 			Events.AddPopupTextEvent(vUnitPosition, "[COLOR_BLUE]+"..iResearchBonus.." [ICON_RESEARCH] Lost Codices[ENDCOLOR]", 2)
+			Events.AddPopupTextEvent(vUnitPosition, "[COLOR_MAGENTA]+"..iResearchBonus.." [ICON_CULTURE] Lost Codices[ENDCOLOR]", 2.5)
 		end
 	end
 end
@@ -48,5 +30,4 @@ function PositionCalculator(i1, i2)
 	return HexToWorld(ToHexFromGrid(Vector2(i1, i2)))
 end
 
---GameEvents.PlayerDoTurn.Add(RenewalCycleOnHolkan)
 GameEvents.GoodyHutReceivedBonus.Add(HolkansGoodyHuts)
