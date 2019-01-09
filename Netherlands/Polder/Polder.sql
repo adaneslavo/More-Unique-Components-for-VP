32065793e16e9f231caf2ae71bc4ae0c196f5ad6
pineappledan <33136027+pineappledan@users.noreply.github.com>
2019-01-09 16:01:24 -0700

    Update Polder.sql
    Added Koog dummy feature for Polder

--- Netherlands/Polder/Polder.sql
+++ Netherlands/Polder/Polder.sql
@@ -5,19 +5,56 @@
 -- Builds
 --------------------------
 INSERT INTO Builds 
+			(Type,				PrereqTech, Time, ImprovementType, Description, Recommendation, Help, HotKey, EntityEvent, OrderPriority, AltDown, IconIndex, IconAtlas, ShowInPedia, ShowInTechTree)
+SELECT		('BUILD_REMOVE_FEATURE_D_FOR_POLDER'), PrereqTech, Time, ImprovementType, Description, Recommendation, Help, HotKey, EntityEvent, 0, 		AltDown, IconIndex, IconAtlas, 0, 0,  
+FROM Builds WHERE (Type = 'BUILD_REMOVE_MARSH');
+
+INSERT INTO Builds 
 			(Type,				PrereqTech, Time, ImprovementType,			Description, Recommendation, Help,							HotKey, EntityEvent, OrderPriority, IconIndex, IconAtlas)
 SELECT		('BUILD_POLDER_2'), PrereqTech, Time, ('IMPROVEMENT_POLDER_2'), Description, Recommendation, 'TXT_KEY_BUILD_POLDER_HELP',	HotKey, EntityEvent, OrderPriority, IconIndex, IconAtlas 
 FROM Builds WHERE (Type = 'BUILD_POLDER');
 
 UPDATE Builds SET Help = 'TXT_KEY_BUILD_POLDER_HELP' WHERE Type = 'BUILD_POLDER';
 --------------------------
+-- BuildFeatures
+--------------------------
+INSERT INTO BuildFeatures 
+			(BuildType,		FeatureType, PrereqTech, Time, Production, Cost, Remove, RemoveOnly)
+SELECT		('BUILD_REMOVE_FEATURE_D_FOR_POLDER'), FeatureType, PrereqTech, Time, Production, Cost, Remove, RemoveOnly
+FROM BuildFeatures WHERE (BuildType = 'BUILD_REMOVE_MARSH');
+--------------------------
 -- Unit_Builds
 --------------------------
 INSERT INTO Unit_Builds 
 			(UnitType,		BuildType)
-VALUES		('UNIT_WORKER',	'BUILD_POLDER_2');
+VALUES		('UNIT_WORKER',	'BUILD_POLDER_2'),
+		('UNIT_WORKER',	'BUILD_REMOVE_FEATURE_D_FOR_POLDER');
+--------------------------
+-- Build_TechTimeChanges
+--------------------------
+INSERT INTO Build_TechTimeChanges
+	(BuildType, TechType, TimeChange)
+VALUES
+	('BUILD_REMOVE_FEATURE_D_FOR_POLDER', 'TECH_MACHINERY', -200),
+	('BUILD_REMOVE_FEATURE_D_FOR_POLDER', 'TECH_BIOLOGY', -200);
 --==========================================================================================================================
-
+--==========================================================================================================================
+-- FEATURES
+--==========================================================================================================================	
+--------------------------
+-- Feature
+--------------------------
+INSERT INTO Features 
+			(Type,						Description, Civilopedia, Help, ArtDefineTag, StartingLocationWeight,	SeeThrough, Defense, WorldSoundscapeAudioScript, EffectTypeTag, Movement, PortraitIndex, IconAtlas)
+SELECT		('FEATURE_D_FOR_POLDER'),	'TXT_KEY_FEATURE_D_FOR_POLDER', Civilopedia, Help, ArtDefineTag, 0,				SeeThrough, Defense, WorldSoundscapeAudioScript, EffectTypeTag, Movement, PortraitIndex, IconAtlas
+FROM Features WHERE (Type = 'FEATURE_MARSH');
+--------------------------
+-- Feature_YieldChanges
+--------------------------
+INSERT INTO Feature_YieldChanges 
+		(FeatureType, 			YieldType, Yield)
+SELECT		('FEATURE_D_FOR_POLDER'),	YieldType, Yield
+FROM Feature_YieldChanges WHERE (Type = 'FEATURE_MARSH');
 --==========================================================================================================================
 -- IMPROVEMENTS
 --==========================================================================================================================	
