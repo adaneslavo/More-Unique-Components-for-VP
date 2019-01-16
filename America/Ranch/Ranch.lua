0a3b19c8d361be4855bc62f319479169f1541acd
pineappledan <33136027+pineappledan@users.noreply.github.com>
2019-01-16 10:58:42 -0700

    Update Ranch.lua

--- America/Ranch/Ranch.lua
+++ America/Ranch/Ranch.lua
@@ -33,10 +33,10 @@ function OnConstructionClaimResources(iPlayer, iCity, iBuilding)
 				
 		for _, res in ipairs(tEligibleResources) do
 			if iResourceTypeOnTile == res then
---				if iPlotOwner == -1 then
+				if not iPlotOwner == iPlayer then
 					pSpecificPlot:SetOwner(iPlayer, iCity, 1, 1)
 					pPlotClaimer = pPlotClaimer + 1
---				end
+				end
 			end
 		end
 	end
