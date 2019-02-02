9fb2bb5f5ea6b5c7cc8a1f65e0fcb079dd0d8ad6
pineappledan <33136027+pineappledan@users.noreply.github.com>
2019-02-02 11:43:47 -0700

    Update SacrificialCaptives.sql
    added trait_freepromotion to Aztecs

--- Aztecs/SacrificialCaptives/SacrificialCaptives.sql
+++ Aztecs/SacrificialCaptives/SacrificialCaptives.sql
@@ -19,4 +19,26 @@ INSERT INTO UnitPromotions
 			(Type, 										Description, 									Help, 													Sound, 				VisibilityChange,	MovesChange,	AttackMod,	DefenseMod, ExperiencePercent,	IgnoreZOC,	LostWithUpgrade,	CannotBeChosen, PortraitIndex, 	IconAtlas, 						PediaType, 		PediaEntry)
 VALUES		('PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE',	'TXT_KEY_PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE',	'TXT_KEY_PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE_HELP',	'AS2D_IF_LEVELUP',	0,					0,				0,			0,			0,					0,			0,					1, 				0, 				'UNIT_POLAND_PROMOTION_ATLAS', 	'PEDIA_MELEE', 'TXT_KEY_PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE');
 --==========================================================================================================================
+-- TRAITS
+--==========================================================================================================================
+--------------------------------	
+-- Trait_FreePromotionUnitCombats
+--------------------------------
+INSERT INTO Trait_FreePromotionUnitCombats
+	(TraitType, UnitCombatType, PromotionType)
+VALUES
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_RECON', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_ARCHER', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_MELEE', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_SIEGE', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_GUN', 			'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_HELICOPTER', 	'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_NAVALMELEE', 	'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_NAVALRANGED', 	'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_FIGHTER', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_BOMBER', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_MOUNTED', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE'),
+	('TRAIT_CULTURE_FROM_KILLS', 'UNITCOMBAT_ARMOR', 		'PROMOTION_UNIT_AZTEC_HUMAN_SACRIFICE');
+
+--==========================================================================================================================
 --==========================================================================================================================
