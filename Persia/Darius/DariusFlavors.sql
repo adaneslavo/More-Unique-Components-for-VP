c6f4af62a5e3c2de7e429a3639cebbeb41b5e836
pineappledan <33136027+pineappledan@users.noreply.github.com>
2019-02-09 13:52:25 -0700

    Update DariusFlavors.sql
    removed 15% tourism conversion and deletion of GAP length boost
    Removed boost to satrap's court per pop yields

--- Persia/Darius/DariusFlavors.sql
+++ Persia/Darius/DariusFlavors.sql
@@ -6,13 +6,3 @@
 INSERT INTO Leader_Flavors
 			(LeaderType,		FlavorType,			Flavor)
 VALUES		('LEADER_DARIUS',	'FLAVOR_CULTURE',	7);
---==========================================================================================================================
--- TRAIT
---==========================================================================================================================	
--- GoldToGAP
-------------------------------
-UPDATE Traits SET GoldToGAP = '15' WHERE Type = 'TRAIT_ENHANCED_GOLDEN_AGES';
-------------------------------
--- GoldenAgeDurationModifier
-------------------------------
-UPDATE Traits SET GoldenAgeDurationModifier = '0' WHERE Type = 'TRAIT_ENHANCED_GOLDEN_AGES';
